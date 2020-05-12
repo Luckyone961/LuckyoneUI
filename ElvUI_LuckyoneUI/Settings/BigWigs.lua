e53e8955bbe83b6ed9f1d9ac78c963b1acaf9ec4
Luckyone <Fussel.13@web.de>
2020-05-12 20:00:52 +0200

    More BigWigs options

--- ElvUI_LuckyoneUI/Settings/BigWigs.lua
+++ ElvUI_LuckyoneUI/Settings/BigWigs.lua
@@ -65,6 +65,43 @@ function L1UI:GetBigWigsProfile()
                         },
                     },
                 },
+                ["BigWigs_Plugins_Bars"] = {
+                    ["profiles"] = {
+                        [key] = {
+                            ["BigWigsEmphasizeAnchor_y"] = 546.8443322281128,
+                            ["fontSize"] = 11,
+                            ["visibleBarLimit"] = 8,
+                            ["BigWigsAnchor_width"] = 168.0000152587891,
+                            ["BigWigsAnchor_y"] = 251.7336688447231,
+                            ["emphasizeRestart"] = false,
+                            ["nameplateOffsetY"] = 0,
+                            ["BigWigsAnchor_x"] = 449.4218827798431,
+                            ["spacing"] = 2,
+                            ["fill"] = false,
+                            ["interceptMouse"] = false,
+                            ["BigWigsEmphasizeAnchor_height"] = 20.0000171661377,
+                            ["growup"] = false,
+                            ["BigWigsEmphasizeAnchor_width"] = 240,
+                            ["fontName"] = "Expressway",
+                            ["BigWigsAnchor_height"] = 17.99999618530273,
+                            ["emphasizeTime"] = 6,
+                            ["visibleBarLimitEmph"] = 5,
+                            ["interceptKey"] = "SHIFT",
+                            ["BigWigsEmphasizeAnchor_x"] = 216.8890678470179,
+                            ["onlyInterceptOnKeypress"] = true,
+                            ["barStyle"] = "ElvUI",
+                            ["texture"] = "ElvUI Blank",
+                        },
+                    },
+                },
+                ["BigWigs_Plugins_InfoBox"] = {
+                    ["profiles"] = {
+                        [key] = {
+                            ["posx"] = 292.2667633228539,
+                            ["posy"] = 71.71122746225456,
+                        },
+                    },
+                },
                 ["BigWigs_Plugins_Alt Power"] = {
                     ["profiles"] = {
                         [key] = {
@@ -78,6 +115,9 @@ function L1UI:GetBigWigsProfile()
             },
             ["profiles"] = {
                 [key] = {
+                    ["showZoneMessages"] = false,
+                    ["fakeDBMVersion"] = true,
+                    ["flash"] = false,
                 },
             },
         }
