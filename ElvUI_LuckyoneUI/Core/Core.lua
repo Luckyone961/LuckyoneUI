4f1edd40fb7ca9721e44e93eb3e5b76afa3bdaf8
Luckyone <Fussel.13@web.de>
2020-05-12 23:27:27 +0200

    Convert Whitespace: Core

--- ElvUI_LuckyoneUI/Core/Core.lua
+++ ElvUI_LuckyoneUI/Core/Core.lua
@@ -8,56 +8,56 @@ local SetCVar = SetCVar
 --Setup AddOns
 function L1UI:AddonSetup(addon)
 
-    --BigWigs
-    if IsAddOnLoaded('BigWigs') then
-        L1UI:GetBigWigsProfile()
-        E:Print('BigWigs profile has been set.')
-    end
+	--BigWigs
+	if IsAddOnLoaded('BigWigs') then
+		L1UI:GetBigWigsProfile()
+		E:Print('BigWigs profile has been set.')
+	end
 
-    --Details! Damage Meter
-    if IsAddOnLoaded('Details') then
-        L1UI:GetDetailsProfile()
-        E:Print('Details profile has been set.')
-    end
+	--Details! Damage Meter
+	if IsAddOnLoaded('Details') then
+		L1UI:GetDetailsProfile()
+		E:Print('Details profile has been set.')
+	end
 
-    --ProjectAzilroka
-    if IsAddOnLoaded('ProjectAzilroka') then
-        L1UI:GetPAProfile()
-        E:Print('ProjectAzilroka profile has been set.')
-    end
+	--ProjectAzilroka
+	if IsAddOnLoaded('ProjectAzilroka') then
+		L1UI:GetPAProfile()
+		E:Print('ProjectAzilroka profile has been set.')
+	end
 
-    E:StaggeredUpdateAll(nil, true)
+	E:StaggeredUpdateAll(nil, true)
 
-    PluginInstallStepComplete.message = "AddOn profiles have been set"
-    PluginInstallStepComplete:Show()
+	PluginInstallStepComplete.message = "AddOn profiles have been set"
+	PluginInstallStepComplete:Show()
 end
 
 --Setup CVars
 function L1UI:SetupCVars()
-    --Maximize the camera zoom
-    SetCVar("cameraDistanceMaxZoomFactor", 2.6)
-    --Remove ugly colors while dead
-    SetCVar("ffxDeath", 0)
-    --Enable advanced logging
-    SetCVar("advancedCombatLogging", 1)
-    --Chat editbox style
-    SetCVar("chatStyle", classic)
-    --NamePlate setup
-    SetCVar("nameplateMinAlpha", 1)
-    SetCVar("nameplateMinScale", 1)
-    SetCVar("nameplateSelfAlpha", 1)
-    SetCVar("nameplateLargerScale", 0)
-    SetCVar("nameplateSelectedScale", 1)
-    --Overlapping 0 Stacking 1
-    SetCVar("nameplateMotion", 1)
-    --Overlap H and V
-    SetCVar("nameplateOverlapH", 1)
-    SetCVar("nameplateOverlapV", 1.5)
-    --Hardware cursor
-    SetCVar("rawMouseEnable", 1)
-    --Quality of life
-    SetCVar("SpellQueueWindow", 50)
+	--Maximize the camera zoom
+	SetCVar("cameraDistanceMaxZoomFactor", 2.6)
+	--Remove ugly colors while dead
+	SetCVar("ffxDeath", 0)
+	--Enable advanced logging
+	SetCVar("advancedCombatLogging", 1)
+	--Chat editbox style
+	SetCVar("chatStyle", classic)
+	--NamePlate setup
+	SetCVar("nameplateMinAlpha", 1)
+	SetCVar("nameplateMinScale", 1)
+	SetCVar("nameplateSelfAlpha", 1)
+	SetCVar("nameplateLargerScale", 0)
+	SetCVar("nameplateSelectedScale", 1)
+	--Overlapping 0 Stacking 1
+	SetCVar("nameplateMotion", 1)
+	--Overlap H and V
+	SetCVar("nameplateOverlapH", 1)
+	SetCVar("nameplateOverlapV", 1.5)
+	--Hardware cursor
+	SetCVar("rawMouseEnable", 1)
+	--Quality of life
+	SetCVar("SpellQueueWindow", 50)
 
-    PluginInstallStepComplete.message = "CVars have been set"
-    PluginInstallStepComplete:Show()
+	PluginInstallStepComplete.message = "CVars have been set"
+	PluginInstallStepComplete:Show()
 end
