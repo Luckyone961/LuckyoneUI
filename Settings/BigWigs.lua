9010e2e1687bb370457fe98049262d0143d62ced
Luckyone <Fussel.13@web.de>
2020-05-12 03:12:33 +0200

    Bye

--- Settings/BigWigs.lua
+++ Settings/BigWigs.lua
@@ -1,8 +1,6 @@
 local E, L, V, P, G = unpack(ElvUI)
 local MyPluginName = "LuckyoneUI"
 local LUI = E:GetModule("LuckyoneUI");
-local IsAddOnLoaded = IsAddOnLoaded
-local format = string.format
 
 function LUI:GetBigWigsProfile()
     local key = Lucky
