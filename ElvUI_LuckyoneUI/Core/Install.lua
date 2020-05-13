cb9969cddd1b6bdd5abb7e7558e1fa5b43cfdf27
Luckyone <Fussel.13@web.de>
2020-05-14 00:04:15 +0200

    Installer cleanup

--- ElvUI_LuckyoneUI/Core/Install.lua
+++ ElvUI_LuckyoneUI/Core/Install.lua
@@ -26,9 +26,9 @@ L1UI.InstallerData = {
 	tutorialImage = "Interface\\AddOns\\ElvUI_LuckyoneUI\\Media\\Textures\\Clover.tga",
 	Pages = {
 		[1] = function()
-			PluginInstallFrame.SubTitle:SetFormattedText("Welcome to the %s installer.", MyPluginName)
+			PluginInstallFrame.SubTitle:SetFormattedText("Welcome")
 			PluginInstallFrame.Desc1:SetText("This installation process will guide you through a few steps and apply settings to your current ElvUI profile.")
-			PluginInstallFrame.Desc2:SetText("|cffdd0017If you want to be able to go back to your original settings then create a new profile before going through this installation process.|r")
+			PluginInstallFrame.Desc2:SetText("Note: |cff4beb2cMake sure you have a backup of your current profile|r")
 			PluginInstallFrame.Option1:Show()
 			PluginInstallFrame.Option1:SetScript("OnClick", InstallComplete)
 			PluginInstallFrame.Option1:SetText("Skip Process")
@@ -49,15 +49,15 @@ L1UI.InstallerData = {
 		end,
 		[3] = function()
 			PluginInstallFrame.SubTitle:SetFormattedText("AddOn Profiles")
-			PluginInstallFrame.Desc1:SetText("Please click the button below to apply Luckyone's AddOn profiles for e.g BigWigs and ProjectAzilroka.")
-			PluginInstallFrame.Desc2:SetText("|cffdd0017This will override your WTF settings for those specific AddOns, make sure to have a backup of your WTF folder if you want to go back at some point.|r")
+			PluginInstallFrame.Desc1:SetText("Please click the button below to apply Luckyone's profiles for AddOns like\nBigWigs, Details! Damage Meter and ProjectAzilroka.")
+			PluginInstallFrame.Desc2:SetText("Importance: |cff4beb2cOptional|r")
 			PluginInstallFrame.Option1:Show()
 			PluginInstallFrame.Option1:SetScript("OnClick", function() L1UI:AddonSetup('addon') end)
 			PluginInstallFrame.Option1:SetText("Setup AddOns")
 		end,
 		[4] = function()
 			PluginInstallFrame.SubTitle:SetFormattedText("CVars")
-			PluginInstallFrame.Desc1:SetText("Please click the button below to apply Luckyone's CVar setup for maxCameraDistance and other tweaks.")
+			PluginInstallFrame.Desc1:SetText("Please click the button below to apply Luckyone's CVar setup for minor tweaks like\n MaxCameraZoom, ChatStyle and HardwareCursor.")
 			PluginInstallFrame.Desc2:SetText("Importance: |cff4beb2cOptional|r")
 			PluginInstallFrame.Option1:Show()
 			PluginInstallFrame.Option1:SetScript("OnClick", function() L1UI:SetupCVars() end)
@@ -65,8 +65,8 @@ L1UI.InstallerData = {
 		end,
 		[5] = function()
 			PluginInstallFrame.SubTitle:SetText("Installation Complete")
-			PluginInstallFrame.Desc1:SetText("You have completed the installation process.")
-			PluginInstallFrame.Desc2:SetText("Please click the button below in order to finalize the process and automatically reload your UI.")
+			PluginInstallFrame.Desc1:SetText("You have completed the installation process, please click 'Finished' to reload the UI.")
+			PluginInstallFrame.Desc2:SetText("Importance: |cff4beb2cHigh|r")
 			PluginInstallFrame.Option1:Show()
 			PluginInstallFrame.Option1:SetScript("OnClick", InstallComplete)
 			PluginInstallFrame.Option1:SetText("Finished")
