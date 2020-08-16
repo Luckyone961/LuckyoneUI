4051e4d73bd375de3dce3cbc5a01861b19c71fd4
Luckyone <Fussel.13@web.de>
2020-08-16 04:07:31 +0200

    New options layout

--- ElvUI_LuckyoneUI/Core/Options.lua
+++ ElvUI_LuckyoneUI/Core/Options.lua
@@ -5,97 +5,127 @@ local L1UI = E:GetModule("LuckyoneUI");
 local format = string.format
 
 function L1UI:Configtable()
+
 	E.Options.args[MyPluginName] = {
 		order = 100,
-		type = "group",
+		type = 'group',
 		name = format("|cff4beb2c%s|r", MyPluginName),
+		childGroups = 'tab',
 		args = {
 			header1 = {
 				order = 1,
 				type = "header",
 				name = "|cff4beb2cLuckyoneUI|r",
 			},
-			install = {
+			general = {
+				type = 'group',
+				name = 'General',
+				order = 1,
+				args = {
+					install = {
+						order = 1,
+						type = "execute",
+						name = "Install",
+						desc = "Run the installation process.",
+						func = function() E:GetModule("PluginInstaller"):Queue(L1UI.InstallerData); E:ToggleOptionsUI(); end,
+					},
+					private = {
+						order = 2,
+						type = "execute",
+						name = "Apply Private Settings",
+						desc = "This will setup Fonts, Textures and Skins.",
+						func = function() L1UI:SetupPrivate(); end,
+					},
+					npreset = {
+						order = 3,
+						type = "execute",
+						name = "Reset Nameplate CVars",
+						desc = "This will reset Nameplate CVars to default.",
+						func = function() L1UI:NameplateReset(); end,
+					},
+				},
+			},
+			links = {
+				type = 'group',
+				name = 'Links',
 				order = 2,
-				type = "execute",
-				name = "Install",
-				desc = "Run the installation process.",
-				func = function() E:GetModule("PluginInstaller"):Queue(L1UI.InstallerData); E:ToggleOptionsUI(); end,
-			},
-			private = {
-				order = 3,
-				type = "execute",
-				name = "Apply Private Settings",
-				desc = "This will setup Fonts, Textures and Skins.",
-				func = function() L1UI:SetupPrivate(); end,
-			},
-			npreset = {
-				order = 4,
-				type = "execute",
-				name = "Reset Nameplate CVars",
-				desc = "This will reset Nameplate CVars to default.",
-				func = function() L1UI:NameplateReset(); end,
-			},
-			changelog = {
-				order = 5,
-				type = 'input',
-				width = 'full',
-				name = "Changelog:",
-				get = function() return 'https://git.tukui.org/Luckyone/ElvUI_LuckyoneUI/-/blob/master/CHANGELOG.md' end,
-			},
-			issues = {
-				order = 6,
-				type = 'input',
-				width = 'full',
-				name = "Report issues here:",
-				get = function() return 'https://git.tukui.org/Luckyone/ElvUI_LuckyoneUI/-/issues' end,
-			},
-			website = {
-				order = 7,
-				type = 'input',
-				width = 'full',
-				name = "Addon link:",
-				get = function() return 'https://www.tukui.org/addons.php?id=154' end,
-			},
-			discord = {
-				order = 8,
-				type = 'input',
-				width = 'full',
-				name = "Discord:",
-				get = function() return 'https://discord.gg/xRY4bwA' end,
-			},
-			header2 = {
-				order = 9,
-				type = "header",
-				name = "Author",
-			},
-			author = {
-				order = 10,
-				type = "description",
-				name = "|cffFF7D0ALuckyone|r (EU) - LaughingSkull",
-				fontSize = "large",
-			},
-			header3 = {
-				order = 11,
-				type = "header",
-				name = "Credits",
+				args = {
+					changelog = {
+						order = 1,
+						type = 'input',
+						width = 'full',
+						name = "Changelog:",
+						get = function() return 'https://git.tukui.org/Luckyone/ElvUI_LuckyoneUI/-/blob/master/CHANGELOG.md' end,
+					},
+					issues = {
+						order = 2,
+						type = 'input',
+						width = 'full',
+						name = "Report issues here:",
+						get = function() return 'https://git.tukui.org/Luckyone/ElvUI_LuckyoneUI/-/issues' end,
+					},
+					website = {
+						order = 3,
+						type = 'input',
+						width = 'full',
+						name = "Addon link:",
+						get = function() return 'https://www.tukui.org/addons.php?id=154' end,
+					},
+					discord = {
+						order = 4,
+						type = 'input',
+						width = 'full',
+						name = "Discord:",
+						get = function() return 'https://discord.gg/xRY4bwA' end,
+					},
+					guide = {
+						order = 5,
+						type = 'input',
+						width = 'full',
+						name = 'Wowhead Guide:',
+						get = function() return 'https://www.wowhead.com/guide=10680/elvui-luckyoneui-addon-plugin-guide' end,
+					},
+				},
 			},
 			credits = {
-				order = 12,
-				type = "description",
-				name = "|cff0070DEAzilroka|r |cffC41F3BKringel|r |cffFF7D0AMerathilis|r |cffF58CBARepooc|r |cFF8866ccSimpy|r",
-				fontSize = "medium",
-			},
-			header4 = {
-				order = 13,
-				type = "header",
-				name = "Helpers",
-			},
-			thanks = {
-				order = 14,
-				type = "description",
-				name = "|cffe6cc80Gandi Illusion Sniefer Xyf|r",
-				fontSize = "medium",
+				type = 'group',
+				name = 'Credits',
+				order = 3,
+				args = {
+					header1 = {
+						order = 1,
+						type = "header",
+						name = "Author",
+					},
+					author = {
+						order = 2,
+						type = "description",
+						name = "|cffFF7D0ALuckyone|r (EU) - LaughingSkull",
+						fontSize = "large",
+					},
+					header2 = {
+						order = 3,
+						type = "header",
+						name = "Credits",
+					},
+					credits = {
+						order = 4,
+						type = "description",
+						name = "|cff0070DEAzilroka|r |cffC41F3BKringel|r |cffFF7D0AMerathilis|r |cffF58CBARepooc|r |cFF8866ccSimpy|r",
+						fontSize = "medium",
+					},
+					header3 = {
+						order = 5,
+						type = "header",
+						name = "Helpers",
+					},
+					thanks = {
+						order = 6,
+						type = "description",
+						name = "|cffe6cc80Gandi Illusion Sniefer Xyf|r",
+						fontSize = "medium",
+					}
+				}
 			}
 		},
 	}
