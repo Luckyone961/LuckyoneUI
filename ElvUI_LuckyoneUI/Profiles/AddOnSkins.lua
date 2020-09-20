cf7f5bcb234bef41b82f67804eb8e33e18e4ee11
Luckyone <Fussel.13@web.de>
2020-09-20 20:16:22 +0200

    AddOnSkins profile part 2

--- ElvUI_LuckyoneUI/Profiles/AddOnSkins.lua
+++ ElvUI_LuckyoneUI/Profiles/AddOnSkins.lua
@@ -6,16 +6,17 @@ function L1UI:GetASProfile()
 
 	local AS = unpack(AddOnSkins)
 
-	AS.data:SetProfile('Luckyone')
+	AS.data:SetProfile("Luckyone")
 
 	--General
-	AS.db["DBMFont"] = 'Expressway'
-	AS.db["Shadows"] = false
+	AS.db['DBMFont'] = 'Expressway'
+	AS.db['Shadows'] = false
 
 	--Embed
-	AS.db["EmbedBackdrop"] = false
-	AS.db["EmbedBackdropTransparent"] = false
-	AS.db["EmbedRightChat"] = false
-	AS.db["EmbedSystemMessage"] = false
+	AS.db['EmbedBackdrop'] = false
+	AS.db['EmbedBackdropTransparent'] = false
+	AS.db['EmbedOoC'] = false
+	AS.db['EmbedRightChat'] = false
+	AS.db['EmbedSystemMessage'] = false
 
 end
