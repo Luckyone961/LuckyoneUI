77df9c5a526785d044c66f37f5dc73ade0a5c096
Luckyone <Fussel.13@web.de>
2020-09-20 22:48:33 +0200

    Media (Fonts)

--- ElvUI_LuckyoneUI/Media/SharedMedia.lua
+++ ElvUI_LuckyoneUI/Media/SharedMedia.lua
@@ -4,5 +4,8 @@ if LSM == nil then return end
 
 --Fonts
 LSM:Register('font','Alpha Echo', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\alpha_echo.ttf]])
-LSM:Register('font','Expressway', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Expressway.ttf]])
+LSM:Register('font','Grandstander', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Grandstander-VariableFont_wght.ttf]])
+LSM:Register('font','Kufam', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Kufam-VariableFont_wght.ttf]])
 LSM:Register('font','Roboto Light', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Roboto-Light.ttf]])
+LSM:Register('font','SCP Regular', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\SourceCodePro-Regular.ttf]])
+LSM:Register('font','Teko Regular', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Teko-Regular.ttf]])
