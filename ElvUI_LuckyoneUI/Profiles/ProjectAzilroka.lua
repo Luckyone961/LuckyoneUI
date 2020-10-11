7f54ea1ce44cbfcdc6c9c542585653bf7586a62a
Luckyone <Fussel.13@web.de>
2020-10-11 05:38:29 +0200

    PA update (Azil baddie)

--- ElvUI_LuckyoneUI/Profiles/ProjectAzilroka.lua
+++ ElvUI_LuckyoneUI/Profiles/ProjectAzilroka.lua
@@ -11,7 +11,7 @@ function L1UI:GetPAProfile()
 	-- Module toggles
 	PA.db["AuraReminder"]["Enable"] = false
 	PA.db["BigButtons"]["Enable"] = false
-	PA.db["cooldown"]["Enable"] = false
+	PA.db["cooldown"]["enable"] = false
 	PA.db["DragonOverlay"]["Enable"] = false
 	PA.db["EnhancedShadows"]["Enable"] = false
 	PA.db["MouseoverAuras"]["Enable"] = false
@@ -37,20 +37,6 @@ function L1UI:GetPAProfile()
 	PA.db["EnhancedFriendsList"]["NameFontSize"] = 11
 	PA.db["EnhancedFriendsList"]["StatusIconPack"] = "Square"
 	PA.db["EnhancedFriendsList"]["Texture"] = "Solid"
-
-	-- stAddonManager Setup
-	PA.db["stAddonManager"]["ButtonHeight"] = 16
-	PA.db["stAddonManager"]["ButtonWidth"] = 16
-	PA.db["stAddonManager"]["ClassColor"] = true
-	PA.db["stAddonManager"]["EnableRequiredAddons"] = false
-	PA.db["stAddonManager"]["Font"] = "Expressway"
-	PA.db["stAddonManager"]["FontSize"] = 12
-	PA.db["stAddonManager"]["FrameWidth"] = 600
-	PA.db["stAddonManager"]["NumAddOns"] = 17
-
-	-- BrokerLDB Setup
-	PA.db["BrokerLDB"]["FontFlag"] = "OUTLINE"
-	PA.db["BrokerLDB"]["Font"] = "Expressway"
 
 	-- Movers
 	E.db["movers"]["SquareMinimapButtonBarMover"] = "TOPRIGHT,ElvUIParent,TOPRIGHT,-1,-185"
