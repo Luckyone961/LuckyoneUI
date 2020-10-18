cc2f0ba978526589f12031d2fda0a2b994c538c2
Luckyone <Fussel.13@web.de>
2020-10-18 04:01:21 +0200

    UI Scale

--- ElvUI_LuckyoneUI/Core/Core.lua
+++ ElvUI_LuckyoneUI/Core/Core.lua
@@ -157,8 +157,7 @@ end
 
 -- UI Scale
 function L1UI:SetupScale()
-	local pixel = E:PixelBestSize()
 
-	E.global["general"]["UIScale"] = pixel
-	SetCVar('uiScale', pixel)
+	E.global["general"]["UIScale"] = 0.71111111111111
+	SetCVar('uiScale', 0.71111111111111)
 end
