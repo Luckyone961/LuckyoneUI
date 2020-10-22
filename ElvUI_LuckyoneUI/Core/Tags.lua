f1a7f63ae26f16d3beab76f0147f96992ced6f71
Luckyone <Fussel.13@web.de>
2020-10-22 23:52:10 +0200

    Tags

---
+++ ElvUI_LuckyoneUI/Core/Tags.lua
@@ -0,0 +1,15 @@
+local L1UI, E, L, V, P, G = unpack(select(2, ...))
+
+--------------------------------------------------
+---------- Copy Paste Stuff for Testing ----------
+--------------------------------------------------
+
+-- ElvUF.Tags.Events['TAG NAME'] = 'EVENTS (SEPERATED W/ SPACE)'
+
+-- ElvUF.Tags.Methods['TAG NAME'] = function()
+
+-- E:AddTagInfo('TAG NAME', 'CATEGORY', 'DESCRIPTION')
+
+--------------------------------------------------
+---------- Copy Paste Stuff for Testing ----------
+--------------------------------------------------
