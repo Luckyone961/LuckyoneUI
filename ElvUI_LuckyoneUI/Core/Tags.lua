eae2485527eb772843d0db35d6abc62eab00675f
Luckyone <Fussel.13@web.de>
2020-10-23 01:26:37 +0200

    New Tag: [luckyone:classification] (classification text without 'Affix')

--- ElvUI_LuckyoneUI/Core/Tags.lua
+++ ElvUI_LuckyoneUI/Core/Tags.lua
@@ -1,15 +1,20 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
---------------------------------------------------
----------- Copy Paste Stuff for Testing ----------
---------------------------------------------------
+local ElvUF = E.oUF
 
--- ElvUF.Tags.Events['TAG NAME'] = 'EVENTS (SEPERATED W/ SPACE)'
+-- Modified verion of oUF classification
+ElvUF.Tags.Events['luckyone:classification'] = 'UNIT_CLASSIFICATION_CHANGED'
+ElvUF.Tags.Methods['luckyone:classification'] = function(u)
+	local c = UnitClassification(u)
+	if (c =='rare') then
+		return 'Rare'
+	elseif (c == 'rareelite') then
+		return 'Rare Elite'
+	elseif (c == 'elite') then
+		return 'Elite'
+	elseif (c == 'worldboss') then
+		return 'Boss'
+	end
+end
 
--- ElvUF.Tags.Methods['TAG NAME'] = function()
-
--- E:AddTagInfo('TAG NAME', 'CATEGORY', 'DESCRIPTION')
-
---------------------------------------------------
----------- Copy Paste Stuff for Testing ----------
---------------------------------------------------
+E:AddTagInfo('luckyone:classification', L1UI.Name, "Displays the unit's classification (e.g 'Elite' and 'Rare') but without 'Affix'")
