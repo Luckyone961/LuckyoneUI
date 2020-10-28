57e0144f08a8ba4d4a90986f721b5586331c6fee
Luckyone <Fussel.13@web.de>
2020-10-28 03:20:20 +0100

    Themes

--- ElvUI_LuckyoneUI/Core/Themes.lua
+++ ElvUI_LuckyoneUI/Core/Themes.lua
@@ -4,12 +4,37 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 function L1UI:SetupTheme(theme)
 
 	if theme == 'dark' then
-		-- DB stuff
+
+		E.db["unitframe"]["colors"]["classbackdrop"] = true
+		E.db["unitframe"]["colors"]["health"]["b"] = 0.058823529411765
+		E.db["unitframe"]["colors"]["health"]["g"] = 0.058823529411765
+		E.db["unitframe"]["colors"]["health"]["r"] = 0.058823529411765
+		E.db["unitframe"]["colors"]["healthclass"] = false
+		E.db["unitframe"]["colors"]["healthMultiplier"] = 0.75
+		E.db["unitframe"]["colors"]["transparentAurabars"] = true
+		E.db["unitframe"]["colors"]["transparentHealth"] = true
+		E.db["unitframe"]["colors"]["transparentPower"] = true
+		E.db["unitframe"]["colors"]["useDeadBackdrop"] = true
+		E.db["unitframe"]["statusbar"] = "Solid"
+
 	elseif theme == 'class' then
-		-- DB stuff
+
+		E.db["unitframe"]["colors"]["classbackdrop"] = false
+		E.db["unitframe"]["colors"]["customhealthbackdrop"] = true
+		E.db["unitframe"]["colors"]["health_backdrop"]["b"] = 0.10196078431373
+		E.db["unitframe"]["colors"]["health_backdrop"]["g"] = 0.10196078431373
+		E.db["unitframe"]["colors"]["health_backdrop"]["r"] = 0.10196078431373
+		E.db["unitframe"]["colors"]["healthclass"] = true
+		E.db["unitframe"]["colors"]["healthMultiplier"] = 0
+		E.db["unitframe"]["colors"]["transparentAurabars"] = true
+		E.db["unitframe"]["colors"]["transparentHealth"] = true
+		E.db["unitframe"]["colors"]["transparentPower"] = true
+		E.db["unitframe"]["colors"]["useDeadBackdrop"] = true
+		E.db["unitframe"]["statusbar"] = "Solid"
+
 	end
 
 	E:StaggeredUpdateAll(nil, true)
 
-	L1UI:Print('AddOnSkins profile has been set.')
+	L1UI:Print('Theme has been set.')
 end
