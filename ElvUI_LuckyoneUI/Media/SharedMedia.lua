141a0df0ea14d91116d043c67571e5943cb85eab
Luckyone <Fussel.13@web.de>
2020-11-05 01:10:38 +0100

    Added Atrocity & Skullflower statusbar textures

--- ElvUI_LuckyoneUI/Media/SharedMedia.lua
+++ ElvUI_LuckyoneUI/Media/SharedMedia.lua
@@ -11,3 +11,7 @@ LSM:Register('font','Kufam', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Kuf
 LSM:Register('font','Meteora', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Meteora.ttf]])
 LSM:Register('font','Roboto Light', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Roboto Light.ttf]])
 LSM:Register('font','Teko', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Teko.ttf]])
+
+-- Statusbar
+LSM:Register('statusbar','Atrocity', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Statusbar\Atrocity.tga]])
+LSM:Register('statusbar','Skullflower', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Statusbar\Skullflower.tga]])
