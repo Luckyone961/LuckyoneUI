470b95c8871a1ff8546d1a039def58be7bf79ca6
Luckyone <Fussel.13@web.de>
2020-11-05 01:02:29 +0100

    Update SharedMedia.lua

--- ElvUI_LuckyoneUI/Media/SharedMedia.lua
+++ ElvUI_LuckyoneUI/Media/SharedMedia.lua
@@ -3,9 +3,11 @@ local LSM = LibStub("LibSharedMedia-3.0")
 if LSM == nil then return end
 
 -- Fonts
-LSM:Register('font','Alpha Echo', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\alpha_echo.ttf]])
-LSM:Register('font','Grandstander', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Grandstander-VariableFont_wght.ttf]])
-LSM:Register('font','Kufam', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Kufam-VariableFont_wght.ttf]])
-LSM:Register('font','Roboto Light', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Roboto-Light.ttf]])
-LSM:Register('font','SCP Regular', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\SourceCodePro-Regular.ttf]])
-LSM:Register('font','Teko Regular', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Teko-Regular.ttf]])
+LSM:Register('font','Alpha Echo', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Alpha Echo.ttf]])
+LSM:Register('font','BeeLeave', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\BeeLeave.ttf]])
+LSM:Register('font','Dreamwood', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Dreamwood.ttf]])
+LSM:Register('font','Handa', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Handa.ttf]])
+LSM:Register('font','Kufam', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Kufam.ttf]])
+LSM:Register('font','Meteora', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Meteora.ttf]])
+LSM:Register('font','Roboto Light', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Roboto Light.ttf]])
+LSM:Register('font','Teko', [[Interface\AddOns\ElvUI_LuckyoneUI\Media\Fonts\Teko.ttf]])
