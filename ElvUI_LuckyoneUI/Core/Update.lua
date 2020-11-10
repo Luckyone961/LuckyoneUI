f93fe157988999b00e7cc01650e6c1a3b226b961
Luckyone <Fussel.13@web.de>
2020-11-10 02:02:02 +0100

    Clean this up for upcoming changes

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -2,17 +2,6 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
 function L1UI:UpdateLayout(layout)
 
-	E.db["auras"]["buffs"]["countFont"] = "Expressway"
-	E.db["auras"]["buffs"]["countFontOutline"] = "OUTLINE"
-	E.db["auras"]["buffs"]["timeFont"] = "Expressway"
-	E.db["auras"]["buffs"]["timeFontOutline"] = "OUTLINE"
-	E.db["auras"]["buffs"]["timeFontSize"] = 12
-	E.db["auras"]["debuffs"]["countFont"] = "Expressway"
-	E.db["auras"]["debuffs"]["countFontOutline"] = "OUTLINE"
-	E.db["auras"]["debuffs"]["timeFont"] = "Expressway"
-	E.db["auras"]["debuffs"]["timeFontOutline"] = "OUTLINE"
-	E.db["auras"]["debuffs"]["timeFontSize"] = 12
-
 	if layout == 'dps' then
 		-- DPS/TANK db
 	elseif layout == 'healer' then
