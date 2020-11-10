b4527e703b00ed79f28706bc2c94f1dd8db96758
Luckyone <Fussel.13@web.de>
2020-11-10 17:56:03 +0100

    Update.lua DPS/TANK

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -2,8 +2,14 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
 function L1UI:UpdateLayout(layout)
 
+	E.db["general"]["talkingHeadFrameScale"] = 0.72
+
 	if layout == 'dps' then
 		-- DPS/TANK db
+		E.db["movers"]["TalkingHeadFrameMover"] = "BOTTOMRIGHT,ElvUIParent,BOTTOMRIGHT,-2,174"
+		E.db["unitframe"]["units"]["pet"]["debuffs"]["priority"] = "Blacklist,Personal,nonPersonal"
+		E.db["unitframe"]["units"]["player"]["castbar"]["customTextFont"]["font"] = "Expressway"
+		E.db["unitframe"]["units"]["player"]["castbar"]["customTimeFont"]["font"] = "Expressway"
 	elseif layout == 'healer' then
 		-- Healing db
 	end
