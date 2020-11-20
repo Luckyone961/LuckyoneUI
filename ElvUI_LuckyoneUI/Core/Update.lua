0ee57306f7e1cc19e7c3ebf7ed94b33e60e29c91
Luckyone <Fussel.13@web.de>
2020-11-20 19:24:55 +0100

    Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -2,10 +2,10 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
 function L1UI:UpdateLayout(layout)
 
-	-- General db
-
 	if layout == 'dps' then
 		-- DPS/TANK db
+		E.db["unitframe"]["units"]["targettarget"]["debuffs"]["maxDuration"] = 0
+		E.db["unitframe"]["units"]["focustarget"]["debuffs"]["maxDuration"] = 0
 	elseif layout == 'healer' then
 		-- Healing db
 	end
