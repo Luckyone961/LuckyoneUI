8ebfc92cb8b260d71f89856c60dbc1728ecdc7c2
Luckyone <Fussel.13@web.de>
2020-12-01 16:07:13 +0100

    Disabled PartyIndicator on Player Frame

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -6,8 +6,10 @@ function L1UI:UpdateLayout(layout)
 
 	if layout == 'dps' then
 		-- DPS/TANK db
+		E.db["unitframe"]["units"]["player"]["partyIndicator"]["enable"] = false
 	elseif layout == 'healer' then
 		-- Healing db
+		E.db["unitframe"]["units"]["player"]["partyIndicator"]["enable"] = false
 	end
 
 	E:StaggeredUpdateAll(nil, true)
