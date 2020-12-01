264bb6c1b65771da09135f6bbe827697903f30b2
Luckyone <Fussel.13@web.de>
2020-12-01 15:56:39 +0100

    Global DB

--- ElvUI_LuckyoneUI/Core/Core.lua
+++ ElvUI_LuckyoneUI/Core/Core.lua
@@ -175,6 +175,8 @@ function L1UI:SetupGlobal()
 		E.global["datatexts"]["customPanels"]["Luckyone_ActionBars_DT"]["visibility"] = "[petbattle] hide;show"
 		E.global["datatexts"]["customPanels"]["Luckyone_ActionBars_DT"]["width"] = 358
 	end
+
+	E.global["datatexts"]["settings"]["Currencies"]["tooltipData"][8][4] = true
 end
 
 -- UI Scale
