c9626ebff79199c420a5b30cf105a6f4adc9201c
Luckyone <Fussel.13@web.de>
2020-12-08 21:27:02 +0100

    Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -7,17 +7,12 @@ function L1UI:UpdateLayout(layout)
 	end
 
 	--General db
-	E.db["general"]["backdropfadecolor"]["a"] = 0.75
-
-	E.db["movers"]["TopCenterContainerMover"] = "TOP,ElvUIParent,TOP,0,-37"
-	E.db["movers"]["BelowMinimapContainerMover"] = "TOP,ElvUIParent,TOP,0,-57"
+	E.db["tooltip"]["healthBar"]["statusPosition"] = "DISABLED"
 
 	if layout == 'dps' then
 		-- DPS/TANK db
-		E.db["unitframe"]["units"]["player"]["partyIndicator"]["enable"] = false
 	elseif layout == 'healer' then
 		-- Healing db
-		E.db["unitframe"]["units"]["player"]["partyIndicator"]["enable"] = false
 	end
 
 	E:StaggeredUpdateAll(nil, true)
