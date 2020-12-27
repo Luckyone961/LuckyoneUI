02a34aa0b1392142dc1bb247005452a250f1d288
Luckyone <Fussel.13@web.de>
2020-12-27 11:47:34 +0100

    SetupBuffs / SetupDebuffs [WIP]

--- ElvUI_LuckyoneUI/Modules/Auras.lua
+++ ElvUI_LuckyoneUI/Modules/Auras.lua
@@ -1,1 +1,41 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
+
+-- Filters: Buffs
+function L1UI:SetupBuffs(frame)
+
+	local all = "Blacklist,Personal,nonPersonal"
+
+	if frame == 'player' then
+
+	elseif frame == 'target' then
+
+	elseif frame == 'focus' then
+
+	elseif frame == 'nameplate' then
+
+	end
+
+	E:StaggeredUpdateAll(nil, true)
+
+	L1UI:Print('Buff filters updated.')
+end
+
+-- Filters: Debuffs
+function L1UI:SetupDebuffs(frame)
+
+	local all = "Blacklist,Personal,nonPersonal"
+
+	if frame == 'player' then
+
+	elseif frame == 'target' then
+
+	elseif frame == 'focus' then
+
+	elseif frame == 'nameplate' then
+
+	end
+
+	E:StaggeredUpdateAll(nil, true)
+
+	L1UI:Print('Debuff filters updated.')
+end
