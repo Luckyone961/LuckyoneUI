26ff39324be6d6a7652eec1cc3f7d486b344132e
Luckyone <Fussel.13@web.de>
2020-12-27 01:23:35 +0100

    Raidframe style options finished
    (Mera OCD?)

--- ElvUI_LuckyoneUI/Core/Themes.lua
+++ ElvUI_LuckyoneUI/Core/Themes.lua
@@ -1,5 +1,7 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
+local ReloadUI = ReloadUI
+
 -- Option to choose between Dark (Default) and Class color
 function L1UI:SetupTheme(theme)
 
@@ -78,23 +80,47 @@ function L1UI:RaidFrames(style)
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["desaturate"] = false
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["maxDuration"] = 0
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["priority"] = "Blacklist,RaidDebuffs,Dispellable"
+		E.db["unitframe"]["units"]["raid"]["groupSpacing"] = 6
+		E.db["unitframe"]["units"]["raid"]["groupsPerRowCol"] = 6
+		E.db["unitframe"]["units"]["raid"]["growthDirection"] = "DOWN_RIGHT"
+		E.db["unitframe"]["units"]["raid"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 		E.db["unitframe"]["units"]["raid"]["health"]["text_format"] = ""
-		E.db["unitframe"]["units"]["raid"]["height"] = 35
-		E.db["unitframe"]["units"]["raid"]["horizontalSpacing"] = 1
+		E.db["unitframe"]["units"]["raid"]["height"] = 24
+		E.db["unitframe"]["units"]["raid"]["horizontalSpacing"] = -1
+		E.db["unitframe"]["units"]["raid"]["name"]["attachTextTo"] = "Frame"
+		E.db["unitframe"]["units"]["raid"]["name"]["position"] = "LEFT"
 		E.db["unitframe"]["units"]["raid"]["name"]["text_format"] = ""
 		E.db["unitframe"]["units"]["raid"]["numGroups"] = 6
 		E.db["unitframe"]["units"]["raid"]["phaseIndicator"]["anchorPoint"] = "LEFT"
 		E.db["unitframe"]["units"]["raid"]["phaseIndicator"]["scale"] = 0.5
 		E.db["unitframe"]["units"]["raid"]["power"]["enable"] = false
+		E.db["unitframe"]["units"]["raid"]["raidicon"]["attachTo"] = "RIGHT"
+		E.db["unitframe"]["units"]["raid"]["raidicon"]["attachToObject"] = "Health"
+		E.db["unitframe"]["units"]["raid"]["raidicon"]["size"] = 12
+		E.db["unitframe"]["units"]["raid"]["raidicon"]["xOffset"] = -15
+		E.db["unitframe"]["units"]["raid"]["raidicon"]["yOffset"] = 0
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["font"] = "Expressway"
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["fontOutline"] = "OUTLINE"
-		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["size"] = 18
-		E.db["unitframe"]["units"]["raid"]["roleIcon"]["enable"] = false
+		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["size"] = 23
+		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["stack"]["position"] = "CENTER"
+		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["stack"]["yOffset"] = 0
+		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["xOffset"] = 82
+		E.db["unitframe"]["units"]["raid"]["readycheckIcon"]["attachTo"] = "Frame"
+		E.db["unitframe"]["units"]["raid"]["readycheckIcon"]["xOffset"] = 48
+		E.db["unitframe"]["units"]["raid"]["resurrectIcon"]["size"] = 25
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["attachTo"] = "Frame"
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["damager"] = false
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["position"] = "RIGHT"
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["size"] = 14
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["xOffset"] = -5
+		E.db["unitframe"]["units"]["raid"]["roleIcon"]["yOffset"] = 0
 		E.db["unitframe"]["units"]["raid"]["summonIcon"]["attachTo"] = "LEFT"
+		E.db["unitframe"]["units"]["raid"]["summonIcon"]["size"] = 25
+		E.db["unitframe"]["units"]["raid"]["summonIcon"]["xOffset"] = 35
 		E.db["unitframe"]["units"]["raid"]["threatStyle"] = "NONE"
 		E.db["unitframe"]["units"]["raid"]["verticalSpacing"] = 1
 		E.db["unitframe"]["units"]["raid"]["visibility"] = "[@raid6,noexists][@raid31,exists] hide;show"
-		E.db["unitframe"]["units"]["raid"]["width"] = 90
+		E.db["unitframe"]["units"]["raid"]["width"] = 140
 	
 	elseif style == 'block' then
 
@@ -119,15 +145,12 @@ function L1UI:RaidFrames(style)
 		-- Setup
 		E.db["unitframe"]["units"]["raid"]["classbar"]["fill"] = "spaced"
 		E.db["unitframe"]["units"]["raid"]["classbar"]["height"] = 6
-		E.db["unitframe"]["units"]["raid"]["debuffs"]["anchorPoint"] = "BOTTOM"
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["countFont"] = "Expressway"
-		E.db["unitframe"]["units"]["raid"]["debuffs"]["countFontSize"] = 14
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["desaturate"] = false
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["maxDuration"] = 0
 		E.db["unitframe"]["units"]["raid"]["debuffs"]["priority"] = "Blacklist,RaidDebuffs,Dispellable"
-		E.db["unitframe"]["units"]["raid"]["debuffs"]["sizeOverride"] = 16
-		E.db["unitframe"]["units"]["raid"]["debuffs"]["spacing"] = 0
-		E.db["unitframe"]["units"]["raid"]["debuffs"]["yOffset"] = 16
+		E.db["unitframe"]["units"]["raid"]["groupSpacing"] = 0
+		E.db["unitframe"]["units"]["raid"]["groupsPerRowCol"] = 1
 		E.db["unitframe"]["units"]["raid"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 		E.db["unitframe"]["units"]["raid"]["health"]["text_format"] = ""
 		E.db["unitframe"]["units"]["raid"]["height"] = 40
@@ -142,13 +165,12 @@ function L1UI:RaidFrames(style)
 		E.db["unitframe"]["units"]["raid"]["raidicon"]["size"] = 12
 		E.db["unitframe"]["units"]["raid"]["raidicon"]["xOffset"] = -15
 		E.db["unitframe"]["units"]["raid"]["raidicon"]["yOffset"] = 0
-		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["duration"]["color"]["b"] = 0.098039215686274
-		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["duration"]["color"]["g"] = 0.070588235294118
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["font"] = "Expressway"
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["fontOutline"] = "OUTLINE"
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["size"] = 18
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["stack"]["position"] = "CENTER"
 		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["stack"]["yOffset"] = 0
+		E.db["unitframe"]["units"]["raid"]["rdebuffs"]["xOffset"] = 0
 		E.db["unitframe"]["units"]["raid"]["readycheckIcon"]["attachTo"] = "Frame"
 		E.db["unitframe"]["units"]["raid"]["readycheckIcon"]["xOffset"] = 48
 		E.db["unitframe"]["units"]["raid"]["resurrectIcon"]["size"] = 25
@@ -168,5 +190,5 @@ function L1UI:RaidFrames(style)
 
 	E:StaggeredUpdateAll(nil, true)
 
-	L1UI:Print('Raidframe style set.')
+	ReloadUI()
 end
