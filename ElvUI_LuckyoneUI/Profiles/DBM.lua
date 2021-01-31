90d5710ee6a8691d78026c27650b4a2ed7a79722
Luckyone <Fussel.13@web.de>
2021-01-31 01:44:55 +0100

    Comments for later

--- ElvUI_LuckyoneUI/Profiles/DBM.lua
+++ ElvUI_LuckyoneUI/Profiles/DBM.lua
@@ -3,4 +3,10 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- DBM profile setup
 function L1UI:GetDBMProfile()
 
+	-- DBM:CreateProfile("Luckyone")
+
+	-- DBM_AllSavedOptions = {}
+
+	-- DBM:ApplyProfile("Luckyone")
+
 end
