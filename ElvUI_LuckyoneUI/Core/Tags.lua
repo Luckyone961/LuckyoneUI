468961b59f9c743c5ae04bb529d980488631a2ed
Luckyone <Fussel.13@web.de>
2021-02-10 15:28:55 +0100

    healermana:current & healermana:percent

--- ElvUI_LuckyoneUI/Core/Tags.lua
+++ ElvUI_LuckyoneUI/Core/Tags.lua
@@ -2,19 +2,48 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
 local ElvUF = E.oUF
 
--- Modified verion of oUF classification
+--[[
+	ref: elvui\ElvUI\Libraries\oUF\elements\tags.lua
+]]
+
+-- Classification without Affix
 ElvUF.Tags.Events['luckyone:classification'] = 'UNIT_CLASSIFICATION_CHANGED'
-ElvUF.Tags.Methods['luckyone:classification'] = function(u)
-	local c = UnitClassification(u)
-	if (c =='rare') then
+ElvUF.Tags.Methods['luckyone:classification'] = function(unit)
+	local class = UnitClassification(unit)
+	if (class == 'rare') then
 		return 'Rare'
-	elseif (c == 'rareelite') then
+	elseif (class == 'rareelite') then
 		return 'Rare Elite'
-	elseif (c == 'elite') then
+	elseif (class == 'elite') then
 		return 'Elite'
-	elseif (c == 'worldboss') then
+	elseif (class == 'worldboss') then
 		return 'Boss'
 	end
 end
 
+-- Healer Mana Current
+ElvUF.Tags.Events['luckyone:healermana:current'] = 'UNIT_POWER_FREQUENT UNIT_MAXPOWER'
+ElvUF.Tags.Methods['luckyone:healermana:current'] = function(unit)
+	local role = UnitGroupRolesAssigned(unit)
+	if (role == 'HEALER') then
+		return UnitPower(unit, Enum.PowerType.Mana)
+	end
+end
+
+-- Healer Mana Percent
+ElvUF.Tags.Events['luckyone:healermana:percent'] = 'UNIT_MAXPOWER UNIT_POWER_FREQUENT'
+ElvUF.Tags.Methods['luckyone:healermana:percent'] = function(unit)
+	local role = UnitGroupRolesAssigned(unit)
+	if (role == 'HEALER') then
+		local max = UnitPowerMax(unit, Enum.PowerType.Mana)
+		if (max == 0) then
+			return 0
+		else
+			return format("%.1f", UnitPower(unit, Enum.PowerType.Mana) / max * 100)
+		end
+	end
+end
+
 E:AddTagInfo('luckyone:classification', L1UI.Name, "Displays the unit's classification (e.g 'Elite' and 'Rare') but without 'Affix'")
+E:AddTagInfo('luckyone:healermana:current', L1UI.Name, "Displays the unit's Mana (Role: Healer)")
+E:AddTagInfo('luckyone:healermana:percent', L1UI.Name, "Displays the unit's Mana in percent (Role: Healer)")
