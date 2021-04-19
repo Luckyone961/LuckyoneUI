94f8fa686f38a49c972e12d6e8de82bb620f19c1
Luckyone <Fussel.13@web.de>
2021-04-19 22:27:14 +0200

    Update.lua/Cleanup

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -6,27 +6,21 @@ function L1UI:UpdateLayout(layout)
 
 	if not E.db.movers then E.db.movers = {} end
 
+	E.db["datatexts"]["fontOutline"] = "OUTLINE"
 	E.db["unitframe"]["units"]["arena"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["arena"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["focus"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["focus"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["party"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["party"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["pet"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["pet"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["player"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["player"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["raid"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["raid"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["raid40"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["raid40"]["healPrediction"]["enable"] = true
-
 	E.db["unitframe"]["units"]["target"]["healPrediction"]["absorbStyle"] = "WRAPPED"
 	E.db["unitframe"]["units"]["target"]["healPrediction"]["enable"] = true
 
