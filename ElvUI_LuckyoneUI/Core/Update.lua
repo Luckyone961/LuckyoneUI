a4f0e05e2df63579987ae6ecc424119a1c7c3764
Luckyone <Fussel.13@web.de>
2021-05-03 02:21:17 +0200

    Add Shadow&Light update to Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -1,16 +1,20 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
 local ReloadUI = ReloadUI
+local IsAddOnLoaded = IsAddOnLoaded
 
 -- Update Layout Buttons (Ingame Config)
 function L1UI:UpdateLayout(layout)
 
 	if not E.db.movers then E.db.movers = {} end
 
+	-- Shadow&Light db changes
+	if IsAddOnLoaded('ElvUI_SLE') then L1UI:GetSLEProfile() end
+
 	if layout == 'dps' then
-
+		-- DPS/TANK db changes
 	elseif layout == 'healer' then
-
+		-- Healing db changes
 	end
 
 	E:StaggeredUpdateAll(nil, true)
