2fbdedd500b9a944fa619875063aa7562b757f4c
Luckyone <Fussel.13@web.de>
2021-05-10 17:53:05 +0200

    BigWigs TBC final version & don't wipe statsDB

--- ElvUI_LuckyoneUI/Profiles/BigWigs.lua
+++ ElvUI_LuckyoneUI/Profiles/BigWigs.lua
@@ -351,255 +351,191 @@ function L1UI:GetBigWigsProfile()
 			},
 		}
 		BigWigsIconDB = {["hide"] = true}
-		BigWigsStatsDB = {}
 
 		BigWigs.db:SetProfile(Rmain)
 
 	elseif L1UI.TBC then
 
-		BigWigs3DB = {
-			["profileKeys"] = {
-				[E.mynameRealm] = Tmain,
-			},
+		BigWigsClassicDB = {
 			["namespaces"] = {
-				["BigWigs_Plugins_Victory"] = {
+				["profileKeys"] = {
+					[E.mynameRealm] = Tmain,
+				},
+				["BigWigs_Plugins_Victory"] = {},
+				["BigWigs_Plugins_Colors"] = {
 					["profiles"] = {
-						[Tmain] = {
-							["bigwigsMsg"] = true,
-							["blizzMsg"] = false,
-						},
 						[Theal] = {
-							["bigwigsMsg"] = true,
-							["blizzMsg"] = false,
+							["barBackground"] = {
+								["BigWigs_Plugins_Colors"] = {
+									["default"] = {
+										0.0588235294117647, -- [1]
+										0.0588235294117647, -- [2]
+										0.0588235294117647, -- [3]
+										0.25, -- [4]
+									},
+								},
+							},
+						},
+						[Tmain] = {
+							["barBackground"] = {
+								["BigWigs_Plugins_Colors"] = {
+									["default"] = {
+										0.0588235294117647, -- [1]
+										0.0588235294117647, -- [2]
+										0.0588235294117647, -- [3]
+										0.25, -- [4]
+									},
+								},
+							},
 						},
 					},
 				},
-				["BigWigs_Plugins_Alt Power"] = {},
-				["BigWigs_Plugins_Sounds"] = {},
-				["BigWigs_Plugins_AutoReply"] = {
+				["BigWigs_Plugins_Raid Icons"] = {
 					["profiles"] = {
-						[Tmain] = {
-							["exitCombatOther"] = 3,
-							["disabled"] = false,
-							["modeOther"] = 2,
-						},
 						[Theal] = {
-							["exitCombatOther"] = 3,
-							["disabled"] = false,
-							["modeOther"] = 2,
+							["disabled"] = true,
+						},
+						[Tmain] = {
+							["disabled"] = true,
 						},
 					},
 				},
 				["BigWigs_Plugins_BossBlock"] = {},
-				["BigWigs_Plugins_Countdown"] = {
-					["profiles"] = {
-						[Tmain] = {
-							["outline"] = "OUTLINE",
-							["fontName"] = "Expressway",
-							["position"] = {
-								nil, -- [1]
-								nil, -- [2]
-								nil, -- [3]
-								-220, -- [4]
-							},
-						},
-						[Theal] = {
-							["outline"] = "OUTLINE",
-							["fontName"] = "Expressway",
-							["position"] = {
-								nil, -- [1]
-								nil, -- [2]
-								nil, -- [3]
-								-220, -- [4]
-							},
-						},
-					},
-				},
-				["BigWigs_Plugins_AltPower"] = {
-					["profiles"] = {
-						[Tmain] = {
-							["disabled"] = true,
-						},
-						[Theal] = {
-							["disabled"] = true,
-						},
-					},
-				},
-				["BigWigs_Plugins_Colors"] = {},
-				["BigWigs_Plugins_Raid Icons"] = {
-					["profiles"] = {
-						[Tmain] = {
-							["disabled"] = true,
-						},
-						[Theal] = {
-							["disabled"] = true,
-						},
-					},
-				},
-				["BigWigs_Plugins_InfoBox"] = {
-					["profiles"] = {
-						[Tmain] = {
-							["posx"] = 962.8442941480171,
-							["posy"] = 71.71141124165615,
-						},
-						[Theal] = {
-							["posx"] = 962.8442941480171,
-							["posy"] = 71.71141124165615,
-						},
-					},
-				},
 				["BigWigs_Plugins_Bars"] = {
 					["profiles"] = {
+						[Theal] = {
+							["outline"] = "OUTLINE",
+							["BigWigsAnchor_width"] = 167,
+							["BigWigsEmphasizeAnchor_height"] = 21.9999656677246,
+							["fontName"] = "Expressway",
+							["BigWigsAnchor_height"] = 19.9999866485596,
+							["visibleBarLimitEmph"] = 8,
+							["visibleBarLimit"] = 8,
+							["BigWigsAnchor_y"] = 226.133186899287,
+							["spacing"] = 3,
+							["BigWigsEmphasizeAnchor_y"] = 212.622162266571,
+							["texture"] = "Minimalist",
+							["barStyle"] = "ElvUI",
+							["BigWigsAnchor_x"] = 401.066155544904,
+							["BigWigsEmphasizeAnchor_x"] = 861.867295175143,
+							["BigWigsEmphasizeAnchor_width"] = 245.999984741211,
+						},
 						[Tmain] = {
 							["outline"] = "OUTLINE",
-							["BigWigsAnchor_width"] = 169.0000152587891,
-							["nameplateOffsetY"] = 0,
-							["BigWigsAnchor_x"] = 449.422189701927,
-							["BigWigsEmphasizeAnchor_height"] = 20.00000190734863,
+							["BigWigsEmphasizeAnchor_height"] = 21.9999656677246,
+							["BigWigsEmphasizeAnchor_x"] = 590.933564656316,
+							["BigWigsAnchor_width"] = 167,
+							["BigWigsAnchor_y"] = 226.133186899287,
 							["fontName"] = "Expressway",
-							["BigWigsAnchor_height"] = 18.00000190734863,
-							["visibleBarLimitEmph"] = 5,
-							["visibleBarLimit"] = 5,
-							["BigWigsAnchor_y"] = 226.1337294340228,
-							["spacing"] = 3,
-							["emphasizeTime"] = 6,
+							["BigWigsAnchor_height"] = 19.9999866485596,
+							["visibleBarLimit"] = 8,
 							["barStyle"] = "ElvUI",
-							["texture"] = "Minimalist",
-							["emphasizeRestart"] = false,
-							["fontSizeEmph"] = 12,
-							["BigWigsEmphasizeAnchor_x"] = 593.0668980413066,
-							["BigWigsEmphasizeAnchor_y"] = 205.5110509832684,
-							["interceptKey"] = "SHIFT",
-							["BigWigsEmphasizeAnchor_width"] = 273.0001220703125,
-						},
-						[Theal] = {
-							["BigWigsEmphasizeAnchor_y"] = 275.9110743893507,
-							["BigWigsAnchor_width"] = 169.0000152587891,
-							["emphasizeRestart"] = false,
-							["nameplateOffsetY"] = 0,
-							["BigWigsEmphasizeAnchor_height"] = 18.99997901916504,
-							["fontName"] = "Expressway",
-							["BigWigsAnchor_height"] = 18.00000190734863,
-							["visibleBarLimitEmph"] = 5,
-							["visibleBarLimit"] = 5,
-							["BigWigsAnchor_y"] = 226.1337728368017,
-							["emphasizeGrowup"] = true,
+							["BigWigsAnchor_x"] = 450.844021333578,
 							["spacing"] = 3,
-							["emphasizeTime"] = 6,
-							["barStyle"] = "ElvUI",
-							["interceptKey"] = "SHIFT",
+							["BigWigsEmphasizeAnchor_y"] = 226.13344731596,
+							["visibleBarLimitEmph"] = 8,
+							["BigWigsEmphasizeAnchor_width"] = 281.000061035156,
 							["texture"] = "Minimalist",
-							["BigWigsEmphasizeAnchor_width"] = 431.9999694824219,
-							["BigWigsEmphasizeAnchor_x"] = 536.1782681915611,
-							["outline"] = "OUTLINE",
-							["BigWigsAnchor_x"] = 399.6442805104744,
-							["fontSizeEmph"] = 12,
 						},
 					},
 				},
-				["BigWigs_Plugins_Super Emphasize"] = {},
 				["BigWigs_Plugins_Wipe"] = {},
+				["BigWigs_Plugins_Super Emphasize"] = {},
+				["BigWigs_Plugins_Sounds"] = {},
+				["BigWigs_Plugins_InfoBox"] = {
+					["profiles"] = {
+						[Theal] = {
+							["posx"] = 962.845249009151,
+							["posy"] = 71.8225277808051,
+						},
+						[Tmain] = {
+							["posx"] = 962.845249009151,
+							["posy"] = 71.8225277808051,
+						},
+					},
+				},
 				["BigWigs_Plugins_Proximity"] = {
 					["profiles"] = {
-						[Tmain] = {
-							["posx"] = 295.8228370242796,
-							["fontName"] = "Expressway",
-							["width"] = 150.0000610351563,
-							["posy"] = 86.04485885435679,
-							["height"] = 119.9999694824219,
-							["disabled"] = true,
-							["font"] = "Friz Quadrata TT",
-						},
 						[Theal] = {
-							["disabled"] = true,
-							["posx"] = 295.8228370242796,
+							["objects"] = {
+								["ability"] = false,
+								["sound"] = false,
+							},
 							["fontName"] = "Expressway",
-							["width"] = 150.0000610351563,
-							["font"] = "Friz Quadrata TT",
-							["height"] = 119.9999694824219,
-							["posy"] = 86.04485885435679,
+							["width"] = 139.999984741211,
+							["posy"] = 86.0440613282958,
+							["posx"] = 295.821448135357,
 						},
-					},
-				},
-				["BigWigs_Plugins_Messages"] = {
-					["profiles"] = {
 						[Tmain] = {
-							["outline"] = "OUTLINE",
-							["fontSize"] = 24,
-							["emphFontName"] = "Expressway",
-							["emphFontSize"] = 34,
-							["fadetime"] = 2.5,
-							["emphPosition"] = {
-								"TOP", -- [1]
-								"TOP", -- [2]
-								nil, -- [3]
-								-120, -- [4]
+							["objects"] = {
+								["ability"] = false,
+								["sound"] = false,
 							},
+							["posy"] = 86.0440613282958,
+							["height"] = 120.000007629395,
+							["posx"] = 295.821448135357,
 							["fontName"] = "Expressway",
-							["displaytime"] = 3.5,
-							["emphOutline"] = "OUTLINE",
-							["normalPosition"] = {
-								"CENTER", -- [1]
-								"CENTER", -- [2]
-								nil, -- [3]
-								225, -- [4]
-							},
-						},
-						[Theal] = {
-							["outline"] = "OUTLINE",
-							["fontSize"] = 24,
-							["emphFontName"] = "Expressway",
-							["emphFontSize"] = 34,
-							["fadetime"] = 2.5,
-							["emphPosition"] = {
-								"TOP", -- [1]
-								"TOP", -- [2]
-								nil, -- [3]
-								-120, -- [4]
-							},
-							["fontName"] = "Expressway",
-							["displaytime"] = 3.5,
-							["emphOutline"] = "OUTLINE",
-							["normalPosition"] = {
-								"CENTER", -- [1]
-								"CENTER", -- [2]
-								nil, -- [3]
-								225, -- [4]
-							},
+							["width"] = 139.999984741211,
 						},
 					},
 				},
 				["BigWigs_Plugins_Statistics"] = {},
-				["BigWigs_Plugins_Pull"] = {
+				["BigWigs_Plugins_Messages"] = {
 					["profiles"] = {
-						[Tmain] = {
-							["endPullSound"] = "None",
-							["voice"] = "enUS: Default (Female)",
-						},
 						[Theal] = {
-							["voice"] = "enUS: Default (Female)",
-							["endPullSound"] = "None",
+							["outline"] = "OUTLINE",
+							["fontSize"] = 22,
+							["emphFontName"] = "Expressway",
+							["fontName"] = "Expressway",
+							["emphOutline"] = "OUTLINE",
+						},
+						[Tmain] = {
+							["outline"] = "OUTLINE",
+							["fontSize"] = 22,
+							["emphFontName"] = "Expressway",
+							["fontName"] = "Expressway",
+							["emphOutline"] = "OUTLINE",
+						},
+					},
+				},
+				["BigWigs_Plugins_AutoReply"] = {
+					["profiles"] = {
+						[Theal] = {
+							["exitCombatOther"] = 3,
+							["disabled"] = false,
+							["modeOther"] = 2,
+						},
+						[Tmain] = {
+							["exitCombatOther"] = 3,
+							["disabled"] = false,
+							["modeOther"] = 2,
+						},
+					},
+				},
+				["BigWigs_Plugins_Pull"] = {},
+				["BigWigs_Plugins_Countdown"] = {
+					["profiles"] = {
+						[Theal] = {
+							["outline"] = "OUTLINE",
+							["fontName"] = "Expressway",
+						},
+						[Tmain] = {
+							["outline"] = "OUTLINE",
+							["fontName"] = "Expressway",
 						},
 					},
 				},
 			},
-			["global"] = {
-				["watchedMovies"] = {},
-			},
 			["profiles"] = {
 				[Theal] = {
-					["showZoneMessages"] = false,
 					["fakeDBMVersion"] = true,
-					["flash"] = false,
 				},
 				[Tmain] = {
-					["showZoneMessages"] = false,
-					["flash"] = false,
 					["fakeDBMVersion"] = true,
 				},
-				["Default"] = {},
 			},
 		}
+		BigWigsIconClassicDB = {["hide"] = true}
 
 		BigWigs.db:SetProfile(Tmain)
 
@@ -785,7 +721,6 @@ function L1UI:GetBigWigsProfile()
 			},
 		}
 		BigWigsIconClassicDB = {["hide"] = true}
-		BigWigsStatsClassicDB = {}
 
 		BigWigs.db:SetProfile(Cmain)
 
