2886e0550818439d2ab6ab95a561493a61d4e6a0
Luckyone <Fussel.13@web.de>
2021-08-11 16:33:19 +0200

    Fixes for TBC

--- ElvUI_LuckyoneUI/Core/Tags.lua
+++ ElvUI_LuckyoneUI/Core/Tags.lua
@@ -1,5 +1,4 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
-if not L1UI.Retail then return end
 
 local floor = floor
 
@@ -32,29 +31,34 @@ E:AddTag('luckyone:health:percent', 'UNIT_HEALTH UNIT_MAXHEALTH', function(unit)
 	return E:GetFormattedText('PERCENT', currentHealth, maxHealth, percent == 100 and 0 or percent < 10 and 2 or 1, nil)
 end)
 
--- Display mana (current) if the unit is flagged healer
-E:AddTag('luckyone:healermana:current', 'UNIT_POWER_FREQUENT UNIT_MAXPOWER', function(unit)
-	local role = UnitGroupRolesAssigned(unit)
-	if (role == 'HEALER') then
-		return UnitPower(unit, Enum.PowerType.Mana)
-	end
-end)
+if L1UI.Retail then
 
--- Display mana (percent) if the unit is flagged healer
-E:AddTag('luckyone:healermana:percent', 'UNIT_MAXPOWER UNIT_POWER_FREQUENT', function(unit)
-	local role = UnitGroupRolesAssigned(unit)
-	if (role == 'HEALER') then
-		local powerMax = UnitPowerMax(unit, Enum.PowerType.Mana)
-		if (powerMax == 0) then
-			return 0
-		else
-			return floor(UnitPower(unit, Enum.PowerType.Mana) / powerMax * 100 + .5)
+	-- Display mana (current) if the unit is flagged healer
+	E:AddTag('luckyone:healermana:current', 'UNIT_POWER_FREQUENT UNIT_MAXPOWER', function(unit)
+		local role = UnitGroupRolesAssigned(unit)
+		if (role == 'HEALER') then
+			return UnitPower(unit, Enum.PowerType.Mana)
 		end
-	end
-end)
+	end)
+
+	-- Display mana (percent) if the unit is flagged healer
+	E:AddTag('luckyone:healermana:percent', 'UNIT_MAXPOWER UNIT_POWER_FREQUENT', function(unit)
+		local role = UnitGroupRolesAssigned(unit)
+		if (role == 'HEALER') then
+			local powerMax = UnitPowerMax(unit, Enum.PowerType.Mana)
+			if (powerMax == 0) then
+				return 0
+			else
+				return floor(UnitPower(unit, Enum.PowerType.Mana) / powerMax * 100 + .5)
+			end
+		end
+	end)
+
+	-- Descriptions for (Retail-Only) Available Tags
+	E:AddTagInfo('luckyone:healermana:current', L1UI.Name, "Displays the unit's Mana (Role: Healer)")
+	E:AddTagInfo('luckyone:healermana:percent', L1UI.Name, "Displays the unit's Mana in percent (Role: Healer)")
+end
 
 -- Descriptions for Available Tags
-E:AddTagInfo('luckyone:healermana:current', L1UI.Name, "Displays the unit's Mana (Role: Healer)")
-E:AddTagInfo('luckyone:healermana:percent', L1UI.Name, "Displays the unit's Mana in percent (Role: Healer)")
 E:AddTagInfo('luckyone:classification', L1UI.Name, "Displays the unit's classification (e.g 'Elite' and 'Rare') but without 'Affix'")
 E:AddTagInfo('luckyone:health:percent', L1UI.Name, "Displays percentage health with 1 decimal below 100%, 2 decimals below 10% and hides decimals at 100%")
