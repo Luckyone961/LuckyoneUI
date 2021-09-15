c1ee882c6e60075190cefdf95ce157cb4bd7cc67
Luckyone <Luckyone.Development@pm.me>
2021-09-15 18:41:06 +0200

    Update.lua

--- ElvUI_LuckyoneUI/Core/Layout.lua
+++ ElvUI_LuckyoneUI/Core/Layout.lua
@@ -8,9 +8,9 @@ if L1UI.Retail then
 	function L1UI:SetupLayout(layout)
 
 		if layout == 'dps' then
-			E.data:SetProfile('Luckyone DPS/TANK v1.35')
+			E.data:SetProfile('Luckyone DPS/TANK v1.36')
 		elseif layout == 'healer' then
-			E.data:SetProfile('Luckyone Healing v1.35')
+			E.data:SetProfile('Luckyone Healing v1.36')
 		end
 
 		if not E.db.movers then E.db.movers = {} end
@@ -1936,9 +1936,9 @@ elseif L1UI.Classic then
 	function L1UI:SetupLayout(layout)
 
 		if layout == 'dps' then
-			E.data:SetProfile('Luckyone DPS/TANK v1.35')
+			E.data:SetProfile('Luckyone DPS/TANK v1.36')
 		elseif layout == 'healer' then
-			E.data:SetProfile('Luckyone Healing v1.35')
+			E.data:SetProfile('Luckyone Healing v1.36')
 		end
 	
 		if not E.db.movers then E.db.movers = {} end
@@ -3237,9 +3237,9 @@ elseif L1UI.TBC then
 	function L1UI:SetupLayout(layout)
 
 		if layout == 'dps' then
-			E.data:SetProfile('Luckyone DPS/TANK v1.35')
+			E.data:SetProfile('Luckyone DPS/TANK v1.36')
 		elseif layout == 'healer' then
-			E.data:SetProfile('Luckyone Healing v1.35')
+			E.data:SetProfile('Luckyone Healing v1.36')
 		end
 
 		if not E.db.movers then E.db.movers = {} end
--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -1,5 +1,7 @@
 local L1UI, E, L, V, P, G = unpack(select(2, ...))
 
+local IsAddOnLoaded = IsAddOnLoaded
+
 -- Update Layout Buttons (Ingame Config)
 if L1UI.Retail then
 
@@ -7,6 +9,9 @@ if L1UI.Retail then
 
 		-- Protect some rare nil errors
 		if not E.db.movers then E.db.movers = {} end
+
+		-- Shadow & Light changes
+		if IsAddOnLoaded('ElvUI_SLE') then L1UI:Get_ShadowAndLight_Profile() end
 
 		if layout == 'dps' then
 			-- DPS/TANK db changes
