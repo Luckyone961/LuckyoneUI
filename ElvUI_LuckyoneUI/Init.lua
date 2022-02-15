1b98873bced3f804a964514ff4354d7ac46f7ca5
Luckyone <Luckyone.Development@pm.me>
2022-02-15 03:08:14 +0100

    Toons

--- ElvUI_LuckyoneUI/Init.lua
+++ ElvUI_LuckyoneUI/Init.lua
@@ -2,6 +2,8 @@ local E, L, V, P, G = unpack(ElvUI)
 local EP = LibStub('LibElvUIPlugin-1.0')
 local addon, Engine = ...
 
+local format = format
+
 local _G = _G
 local GetAddOnMetadata = GetAddOnMetadata
 
@@ -21,6 +23,22 @@ L1UI.CreditsList = {}
 L1UI.Name = '|cff4beb2cLuckyoneUI|r'
 L1UI.Version = GetAddOnMetadata(addon, 'Version')
 
+local name = format('%s-%s', E.myname, E:ShortenRealm(E.myrealm))
+local toons = {
+	['Luckyone-LaughingSkull'] = true,
+	['Luckypriest-LaughingSkull'] = true,
+	['Luckymonkas-LaughingSkull'] = true,
+	['Luckydk-LaughingSkull'] = true,
+	['Luckyhunter-LaughingSkull'] = true,
+	['Unluckyone-LaughingSkull'] = true,
+	['Notlucky-LaughingSkull'] = true,
+	['Luckymage-LaughingSkull'] = true,
+	['Luckydh-LaughingSkull'] = true,
+	['Luckywl-LaughingSkull'] = true,
+	['Luckyrogue-LaughingSkull'] = true,
+	['Luckypala-LaughingSkull'] = true
+}
+
 function L1UI:PLAYER_ENTERING_WORLD(_, initLogin, isReload)
 
 	if initLogin or not ElvDB.LuckyoneDisabledAddOns then
@@ -38,6 +56,11 @@ end
 function L1UI:Initialize()
 	if E.private.install_complete and E.private.L1UI.install_version == nil then
 		E.PluginInstaller:Queue(L1UI.InstallerData)
+	end
+
+	-- Disable Shadow & Light alpha chat print
+	if toons[name] then
+		ElvDB.ShadowLightAlpha = false
 	end
 
 	EP:RegisterPlugin(addon, L1UI.GetOptions)
