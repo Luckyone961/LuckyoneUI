e31f63350e427c99fd71703099a6f17848d45ca6
Luckyone <Luckyone.Development@pm.me>
2022-03-13 19:29:27 +0100

    Remove :healermana tag for non-retail

--- ElvUI_LuckyoneUI/Core/Layout.lua
+++ ElvUI_LuckyoneUI/Core/Layout.lua
@@ -592,7 +592,7 @@ function L1UI:SetupLayout(layout)
 		["fontOutline"] = "OUTLINE",
 		["justifyH"] = "CENTER",
 		["size"] = 14,
-		["text_format"] = "[classcolor][name][ - >luckyone:healermana:percent<%]",
+		["text_format"] = E.Retail and "[classcolor][name][ - >luckyone:healermana:percent<%]" or "[classcolor][name]",
 		["xOffset"] = 0,
 		["yOffset"] = 0
 	}
