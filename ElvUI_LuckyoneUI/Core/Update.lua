f821c81620ff6285929b3acf867980cd55e418c2
Luckyone <Luckyone.Development@pm.me>
2022-05-06 23:31:00 +0200

    Wipe Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -5,10 +5,5 @@ function L1UI:UpdateLayout(layout)
 
 	E:StaggeredUpdateAll()
 
-	E.db["tooltip"]["colorAlpha"] = 0.80
-	E.db["tooltip"]["headerFont"] = "Expressway"
-	E.db["tooltip"]["headerFontOutline"] = "OUTLINE"
-	E.db["tooltip"]["headerFontSize"] = 11
-
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "]..L1UI.Version)
 end
