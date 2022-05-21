0ff4d7fb73d7bb6422f4c7af904b680acee342e5
Luckyone <Luckyone.Development@pm.me>
2022-05-21 23:46:26 +0200

    Revert "Custom DT Font Size"
    This reverts commit c5a0b92f50864a595c9b998671186e8f0b31e4f2.

--- ElvUI_LuckyoneUI/Core/Core.lua
+++ ElvUI_LuckyoneUI/Core/Core.lua
@@ -181,7 +181,7 @@ function L1UI:SetupGlobal()
 		LuckyDT.fonts.enable = true
 		LuckyDT.fonts.font = "Expressway"
 		LuckyDT.fonts.fontOutline = "OUTLINE"
-		LuckyDT.fonts.fontSize = 10
+		LuckyDT.fonts.fontSize = 11
 		LuckyDT.frameLevel = 1
 		LuckyDT.frameStrata = "BACKGROUND"
 		LuckyDT.growth = "HORIZONTAL"
