5e721bb9b7b4f0f5c53b5f6db81b70dd43f8f9a6
Luckyone <Luckyone.Development@pm.me>
2022-05-31 15:45:56 +0200

    Update.lua CG

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,6 +3,13 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Update Layout Buttons (Ingame Config)
 function L1UI:UpdateLayout(layout)
 
+	E.db["general"]["customGlow"]["color"]["a"] = 1
+	E.db["general"]["customGlow"]["color"]["b"] = 1
+	E.db["general"]["customGlow"]["color"]["g"] = 1
+	E.db["general"]["customGlow"]["color"]["r"] = 1
+	E.db["general"]["customGlow"]["style"] = "Autocast Shine"
+	E.db["general"]["customGlow"]["useColor"] = true
+
 	E:StaggeredUpdateAll()
 
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "]..L1UI.Version)
