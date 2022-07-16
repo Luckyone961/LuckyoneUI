20ebbe81e9c9a89f44f6f0ee592380ce4a6129ab
Luckyone <Luckyone.Development@pm.me>
2022-07-16 22:30:43 +0200

    DefaultFont

--- ElvUI_LuckyoneUI/Init.lua
+++ ElvUI_LuckyoneUI/Init.lua
@@ -48,6 +48,7 @@ local toons = {
 -- Constants
 L1UI.Config = {}
 L1UI.CreditsList = {}
+L1UI.DefaultFont = "Expressway"
 L1UI.Logo = 'Interface\\AddOns\\ElvUI_LuckyoneUI\\Media\\Textures\\Clover.tga'
 L1UI.Me = toons[name]
 L1UI.Name = '|cff4beb2cLuckyoneUI|r'
