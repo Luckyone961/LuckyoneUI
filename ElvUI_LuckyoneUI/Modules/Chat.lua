76a1ac06918fcb84c520a76e8110795c0fec1404
Luckyone <Luckyone.Development@pm.me>
2022-08-23 00:35:22 +0200

    Update Setup_Chat

--- ElvUI_LuckyoneUI/Modules/Chat.lua
+++ ElvUI_LuckyoneUI/Modules/Chat.lua
@@ -10,8 +10,10 @@ local ChatFrame_RemoveAllMessageGroups = ChatFrame_RemoveAllMessageGroups
 local ChatFrame_RemoveMessageGroup = ChatFrame_RemoveMessageGroup
 local FCF_OpenNewWindow = FCF_OpenNewWindow
 local FCF_ResetChatWindows = FCF_ResetChatWindows
+local FCF_SavePositionAndDimensions = FCF_SavePositionAndDimensions
 local FCF_SetChatWindowFontSize = FCF_SetChatWindowFontSize
 local FCF_SetWindowName = FCF_SetWindowName
+local FCF_StopDragging = FCF_StopDragging
 local FCFDock_SelectWindow = FCFDock_SelectWindow
 local FCFTab_UpdateColors = FCFTab_UpdateColors
 local GetLocale = GetLocale
@@ -39,12 +41,6 @@ function L1UI:Setup_Chat()
 	-- Reset chat to Blizzard defaults
 	FCF_ResetChatWindows()
 
-	-- Join LFG channel in Wrath | TBC | Classic (English client only)
-	if not E.Retail and GetLocale() == 'enUS' then
-		JoinPermanentChannel('LookingForGroup')
-		ChatFrame_AddChannel(_G.ChatFrame1, 'LookingForGroup')
-	end
-
 	-- Open 3 new tabs
 	FCF_OpenNewWindow()
 	FCF_OpenNewWindow()
@@ -61,9 +57,11 @@ function L1UI:Setup_Chat()
 		-- Font size 10 for all tabs
 		FCF_SetChatWindowFontSize(nil, frame, 10)
 
-		-- Tab names
+		-- Tabs
 		if id == 1 then
 			FCF_SetWindowName(frame, 'General')
+			frame:ClearAllPoints()
+			frame:Point('BOTTOMLEFT', _G.LeftChatToggleButton, 'TOPLEFT', 1, 3)
 		elseif id == 2 then
 			FCF_SetWindowName(frame, 'Log')
 		elseif id == 3 then
@@ -77,36 +75,39 @@ function L1UI:Setup_Chat()
 		elseif id == 6 then
 			FCF_SetWindowName(frame, 'Party')
 		end
+
+		FCF_SavePositionAndDimensions(frame)
+		FCF_StopDragging(frame)
 	end
 
 	-- Whisper tab
 	local whisper = {'WHISPER', 'BN_WHISPER', 'IGNORED'}
 	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame4)
-	for _, k in ipairs(whisper) do
-		ChatFrame_AddMessageGroup(_G.ChatFrame4, k)
+	for _, v in ipairs(whisper) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame4, v)
 	end
 
 	-- Guild tab
 	local guild = {'GUILD', 'GUILD_ACHIEVEMENT', 'OFFICER'}
 	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame5)
-	for _, k in ipairs(guild) do
-		ChatFrame_AddMessageGroup(_G.ChatFrame5, k)
+	for _, v in ipairs(guild) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame5, v)
 	end
 
 	-- Party tab
 	local party = {'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER'}
 	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame6)
-	for _, k in ipairs(party) do
-		ChatFrame_AddMessageGroup(_G.ChatFrame6, k)
+	for _, v in ipairs(party) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame6, v)
 	end
-
-	-- Jump back to main tab
-	FCFDock_SelectWindow(_G.GENERAL_CHAT_DOCK, _G.ChatFrame1)
 
 	-- Remove whispers from main tab
 	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'IGNORED')
 	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'WHISPER')
 	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'BN_WHISPER')
+
+	-- Select the main tab
+	FCFDock_SelectWindow(_G.GENERAL_CHAT_DOCK, _G.ChatFrame1)
 
 	L1UI:Print(L["Chat setup successful."])
 end
