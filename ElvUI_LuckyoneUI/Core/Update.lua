92f451c016e6a7abd81080131fc9f38362550a40
Luckyone <Luckyone.Development@pm.me>
2022-08-26 03:24:12 +0200

    Update button

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -7,6 +7,11 @@ function L1UI:UpdateLayout(layout)
 	E.db.unitframe.units.boss.raidicon.size = 10
 	E.db.unitframe.units.boss.raidicon.yOffset = -1
 
+	-- Party frame updates
+	if layout == 'main' then
+		E.db.unitframe.units.party.showPlayer = false
+	end
+
 	E.db.L1UI.install_version = L1UI.Version
 
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "]..L1UI.Version)
