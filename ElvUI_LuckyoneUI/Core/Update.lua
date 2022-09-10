26a4ce7afb09578b85c2cb09d806f74f339b08e0
Luckyone <Luckyone.Development@pm.me>
2022-09-10 13:58:39 +0200

    Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,9 +3,20 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Ingame config buttons 'Update Main' & 'Update Healing'
 function L1UI:UpdateLayout(layout)
 
+	-- Turn off new 'module'
+	E.db.cooldown.targetAura = false
+
 	-- UnitFrames updates
 	E.db.unitframe.colors.transparentHealth = true
+	E.db.unitframe.units.raid1.visibility = E.Retail and '[@raid6,noexists][@raid21,exists] hide;show' or '[@raid6,noexists][@raid11,exists] hide;show'
+	E.db.unitframe.units.raid3.visibility = E.Retail and '[@raid31,noexists] hide;show' or '[@raid26,noexists] hide;show'
 
+	-- Match raid1 and raid2
+	E:CopyTable(E.db.unitframe.units.raid2, E.db.unitframe.units.raid1)
+	E.db.unitframe.units.raid2.numGroups = 6
+	E.db.unitframe.units.raid2.visibility = E.Retail and '[@raid21,noexists][@raid31,exists] hide;show' or '[@raid11,noexists][@raid26,exists] hide;show'
+
+	-- Bump install version
 	E.db.L1UI.install_version = L1UI.Version
 
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "]..L1UI.Version)
