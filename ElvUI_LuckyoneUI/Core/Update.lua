6f5d57afe16036b08089bbbebce97e81c54022bd
Luckyone <Luckyone.Development@pm.me>
2022-09-19 09:41:06 +0200

    Include Party Pets in Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,6 +3,17 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Ingame config buttons 'Update Main' & 'Update Healing'
 function L1UI:UpdateLayout(layout)
 
+	if layout == 'healing' then
+		-- Healing Party Pets [nonRetail only]
+		E.db.unitframe.units.party.petsGroup.anchorPoint = 'BOTTOM'
+		E.db.unitframe.units.party.petsGroup.enable = not E.Retail
+		E.db.unitframe.units.party.petsGroup.healPrediction.enable = true
+		E.db.unitframe.units.party.petsGroup.threatStyle = 'NONE'
+		E.db.unitframe.units.party.petsGroup.width = 94
+		E.db.unitframe.units.party.petsGroup.xOffset = 0
+		E.db.unitframe.units.party.petsGroup.yOffset = -1
+	end
+
 	-- Bump install version
 	E.db.L1UI.install_version = L1UI.Version
 
