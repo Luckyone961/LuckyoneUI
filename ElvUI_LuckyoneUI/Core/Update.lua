23b2521fbaa123f532cfec350f080b912f4c91ae
Luckyone <Luckyone.Development@pm.me>
2023-01-09 14:14:29 +0100

    Update.lua

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,6 +3,9 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Ingame config buttons 'Update Main' & 'Update Healing'
 function L1UI:UpdateLayout(layout)
 
+	-- ActionBar db updates
+	E.db.actionbar.microbar.useIcons = false
+
 	-- Auras db updates
 	E.db.auras.cooldown.checkSeconds = true
 	E.db.auras.cooldown.hhmmThreshold = 120
@@ -15,8 +18,10 @@ function L1UI:UpdateLayout(layout)
 	E.db.cooldown.mmssColor.g = 1
 	E.db.cooldown.mmssColor.r = 1
 
-	-- ActionBar db updates
-	E.db.actionbar.microbar.useIcons = false
+	-- NamePlate db updates
+	if E.private.nameplates.enable then
+		E.db.nameplates.units.ENEMY_PLAYER.buffs.priority = 'Blacklist,Dispellable,TurtleBuffs'
+	end
 
 	-- Bump install version
 	E.db.L1UI.install_version = L1UI.Version
