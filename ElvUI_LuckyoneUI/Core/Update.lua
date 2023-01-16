73e5d87370434f592010abb5a0f016541d351c8e
Luckyone <Luckyone.Development@pm.me>
2023-01-16 16:38:10 +0100

    Print it on Update

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -7,4 +7,9 @@ function L1UI:UpdateLayout(layout)
 	E.db.L1UI.install_version = L1UI.Version
 
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "]..L1UI.Version)
+
+	-- ToDo: Remove me before 2.19
+	L1UI:Print('If you are looking for the new NamePlates style please re-apply the profile:')
+	L1UI:Print('For ElvUI NamePlates: in /luckyoneui config -> Profiles tab')
+	L1UI:Print('For Plater: Re-import from https://wago.io/LuckyoneUI-Plater')
 end
