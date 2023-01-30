93b70784eb4c1b53c7ef8d579e3065ff0cf6b56a
Luckyone <Luckyone.Development@pm.me>
2023-01-30 17:10:21 +0100

    Enable RaidIcon for Target and Focus by default

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,21 +3,16 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Ingame config buttons 'Update Main' & 'Update Healing'
 function L1UI:UpdateLayout(layout)
 
-	-- ActionBar db updates
-	if E.Retail then
-		E.db.actionbar.bar1.professionQuality.enable = false
-		E.db.actionbar.bar2.professionQuality.enable = false
-		E.db.actionbar.bar3.professionQuality.enable = false
-		E.db.actionbar.bar4.professionQuality.enable = false
-		E.db.actionbar.bar5.professionQuality.enable = false
-		E.db.actionbar.bar6.professionQuality.enable = false
-		E.db.actionbar.bar7.professionQuality.enable = false
-		E.db.actionbar.bar8.professionQuality.enable = false
-		E.db.actionbar.bar9.professionQuality.enable = false
-		E.db.actionbar.bar10.professionQuality.enable = false
-		E.db.actionbar.bar13.professionQuality.enable = false
-		E.db.actionbar.bar14.professionQuality.enable = false
-		E.db.actionbar.bar15.professionQuality.enable = false
+	-- UnitFrame updates
+	if layout == 'main' then
+		-- Focus
+		E.db.unitframe.units.focus.raidicon.attachTo = 'CENTER'
+		E.db.unitframe.units.focus.raidicon.enable = true
+		E.db.unitframe.units.focus.raidicon.yOffset = 0
+		-- Target
+		E.db.unitframe.units.target.raidicon.attachTo = 'CENTER'
+		E.db.unitframe.units.target.raidicon.enable = true
+		E.db.unitframe.units.target.raidicon.yOffset = 0
 	end
 
 	-- Bump install version
