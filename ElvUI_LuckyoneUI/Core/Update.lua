ab2dc18e3a4b74114def53bfc577595ab7e1723c
Luckyone <Luckyone.Development@pm.me>
2023-02-03 16:24:46 +0100

    Cleanup

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -3,20 +3,6 @@ local L1UI, E, L, V, P, G = unpack(select(2, ...))
 -- Ingame config buttons 'Update Main' & 'Update Healing'
 function L1UI:UpdateLayout(layout)
 
-	-- UnitFrame updates
-	E.db.unitframe.units.focus.raidicon.attachTo = 'CENTER'
-	E.db.unitframe.units.focus.raidicon.enable = true
-	E.db.unitframe.units.focus.raidicon.yOffset = 0
-
-	E.db.unitframe.units.target.raidicon.attachTo = 'CENTER'
-	E.db.unitframe.units.target.raidicon.enable = true
-	E.db.unitframe.units.target.raidicon.yOffset = 0
-
-	-- WindTools updates
-	if E:IsAddOnEnabled('ElvUI_WindTools') and E.Retail then
-		L1UI:Setup_WindTools(true)
-	end
-
 	-- Bump install version
 	E.db.L1UI.install_version = L1UI.Version
 
