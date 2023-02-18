8d3d3d5dddb6c6dc34a4435d5fdda96493a45671
Luckyone <Luckyone.Development@pm.me>
2023-02-18 19:22:41 +0100

    Protect this

--- ElvUI_LuckyoneUI/Core/Update.lua
+++ ElvUI_LuckyoneUI/Core/Update.lua
@@ -5,9 +5,11 @@ function L1UI:UpdateLayout(layout)
 	-- Bump install version
 	E.db.L1UI.install_version = L1UI.Version
 
-	-- General db updates
-	E.db.general.altPowerBar.statusBarColorGradient = true
-	E.db.general.itemLevel.itemLevelFontSize = 10
+	if E.Retail then
+		-- General db updates
+		E.db.general.altPowerBar.statusBarColorGradient = true
+		E.db.general.itemLevel.itemLevelFontSize = 10
+	end
 
 	L1UI:Print(L["Successfully updated your current layout to LuckyoneUI version "] .. L1UI.Version)
 end
