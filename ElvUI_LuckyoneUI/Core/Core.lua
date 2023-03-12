0d2a4fdd4f2fb40cda7f7992e8b77262cd9938e4
Luckyone <Luckyone.Development@pm.me>
2023-03-12 14:43:59 +0100

    Add db for dev toggle

--- ElvUI_LuckyoneUI/Core/Core.lua
+++ ElvUI_LuckyoneUI/Core/Core.lua
@@ -200,6 +200,7 @@ function L1UI:Setup_PrivateDB()
 		E.private.L1UI.disabledFrames.BossBanner = true
 		E.private.L1UI.qualityOfLife.easyDelete = true
 		E.private.L1UI.skins.BugSack = true
+		E.private.L1UI.skins.TalentLoadoutsEx = true
 	end
 end
 
