c54d71973180fecf5b66b74b149d23203a029a79
Luckyone <Luckyone.Development@pm.me>
2023-03-14 13:01:21 +0100

    Use 1 file for defaults instead

---
+++ ElvUI_LuckyoneUI/Settings/Defaults.lua
@@ -0,0 +1,30 @@
+local L1UI, E, L, V, P, G = unpack(select(2, ...))
+
+-- Defaults: E.global.L1UI
+G.L1UI = {
+	dev = false,
+	install_version = nil,
+}
+
+-- Defaults: E.private.L1UI
+V.L1UI = {
+	disabledFrames = {
+		AlertFrame = false,
+		BossBanner = false,
+		ZoneTextFrame = false,
+	},
+	qualityOfLife = {
+		easyDelete = false,
+	},
+	skins = {
+		BugSack = false,
+		TalentLoadoutsEx = false,
+	},
+}
+
+-- Defaults: E.db.L1UI
+P.L1UI = {
+	general = {
+		auto_update = false,
+	},
+}
