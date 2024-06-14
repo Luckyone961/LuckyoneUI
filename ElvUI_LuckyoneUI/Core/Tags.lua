ef1f65c57422227f3b0c34f456eb2751dd5941e1
Luckyone <Luckyone.Development@pm.me>
2024-06-14 21:27:32 +0200

    Update luckyone:power:percent tag

--- ElvUI_LuckyoneUI/Core/Tags.lua
+++ ElvUI_LuckyoneUI/Core/Tags.lua
@@ -27,6 +27,7 @@ local UnitIsWildBattlePet = UnitIsWildBattlePet
 local UnitName = UnitName
 local UnitPower = UnitPower
 local UnitPowerMax = UnitPowerMax
+local UnitPowerType = UnitPowerType
 local UnitReaction = UnitReaction
 
 -- Display unit classification without 'affix' on minor enemies
@@ -45,11 +46,24 @@ end)
 E:AddTagInfo('luckyone:health:percent', Private.Name, L["Displays percentage health with 1 decimal below 100%, 2 decimals below 10% and hides decimals at 100%"])
 
 -- Display percentage power and hides power at 0
-E:AddTag('luckyone:power:percent', 'UNIT_MAXPOWER UNIT_POWER_FREQUENT', function(unit)
+E:AddTag('luckyone:power:percent', 'UNIT_MAXPOWER UNIT_POWER_FREQUENT UNIT_DISPLAYPOWER', function(unit)
 	local min, max = UnitPower(unit), UnitPowerMax(unit)
-	return (min == 0) and '' or floor(UnitPower(unit) / max * 100 + .5)
+	local pType, pToken, altR, altG, altB = UnitPowerType(unit)
+	local color = _COLORS.power[pToken]
+	if (not color) then
+		if (altR) then
+			if (altR > 1 or altG > 1 or altB > 1) then
+				return format('%s%s', Hex(altR / 255, altG / 255, altB / 255), (min == 0) and '' or floor(UnitPower(unit) / max * 100 + .5))
+			else
+				return format('%s%s', Hex(altR, altG, altB), (min == 0) and '' or floor(UnitPower(unit) / max * 100 + .5))
+			end
+		else
+			return format('%s%s', Hex(_COLORS.power[pType] or _COLORS.power.MANA), (min == 0) and '' or floor(UnitPower(unit) / max * 100 + .5))
+		end
+	end
+	return format('%s%s', Hex(color), (min == 0) and '' or floor(UnitPower(unit) / max * 100 + .5))
 end)
-E:AddTagInfo('luckyone:power:percent', Private.Name, L["Displays percentage power and hides power at 0"])
+E:AddTagInfo('luckyone:power:percent', Private.Name, L["Displays percentage power with powercolor and hides power at 0"])
 
 -- Display percentage mana with 0 decimals
 E:AddTag('luckyone:mana:percent', 'UNIT_POWER_FREQUENT UNIT_MAXPOWER UNIT_DISPLAYPOWER', function(unit)
