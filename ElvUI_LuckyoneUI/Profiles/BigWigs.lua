b6381e6f405c8dc9a92cb9794ca32343c0be2d64
Luckyone <Luckyone.Development@pm.me>
2024-07-19 21:22:19 +0200

    Update comment

--- ElvUI_LuckyoneUI/Profiles/BigWigs.lua
+++ ElvUI_LuckyoneUI/Profiles/BigWigs.lua
@@ -16,7 +16,7 @@ function Private:Setup_BigWigs(layout)
 	local profile_healing = 'BW1:DnvVUTnsqyWWyKIdjwsg2U5k8vKding20NDY5Uqd6FeKSeKPJbCt0sQXKlULCxS7sjRGuPQRw1PYiipa6rq1PyHEe8JGqEaYUKI0iHfd4mlNV577BhA9nwTeqiqrqxQalX0uXFphsyXLPnD870vzB99gpMs5jis1bbN4DPVxp1AwFD9fRa5ekHYfddyzCgboUR9E7(wR8OBajROGJj8p29I4aKwuXop6gogvwWoymqi0rhBNF2(buokncoop7W8soCyqrEoIolxXHRaPeNgj()ACAMKGtbENR9BDXLEtUJMkVc)j4VwyK41mgWdrcO)K7qdajob6AV)UopAoSnnLggZPjqCUFCQU1lrjWdE3Z46bncnohotn1wUHXiz8SbybJGgBqATzMU6uqavTzrC6ORzJq8bI4jzcahQT6(pesqcrOX16xngddFtpebhLQw7HKNiY0aeVWH)Wc9RUOW)tdBw6GMDF(E7U3H)suBj)R5R8H7LxfJgqh10YYYMVQutB9ZKsaZbWjV1doWep6D2MM90ucj08zqtBtZdbUqF374EZ(SgH6rl14MwT6S1ew52HY2S0S9mnivxi)PADvTPqjM9xoelWbeWfXBHtWYxonx0(A7RxVlo7C)PW9SZbCuS81Zngw5GCB5DQ)YkGot7TzSyxJV3FIUNBWdKX)4vSAvFspqirCzSRbM(CP2bY4WdTXPy9onwinwXvYXeONhz41xWfmuOM0VyEXw)kwmRiBf(brLdwdxF12QTwjaDQYX9omHeVOIcgzvNTXVPzJd)hQDIv13CE56PP22SnQ6SDgrIzemWTFcU20Hq8YQ7GI9ITMAWi)9JAWTBez1iWAUwAp939mT)uMDBGBhF)oTvT28l2Znc)0p2vVadC1ZVv1sx(Zpt1MvVAkLxL3zUjv1HfvNyuxUo0UqVHuCim3lns7SXhVZ7tg)Z)'
 
 	-- Profile import
-	-- Arg 1: AddOn name, Arg 2: Profile string, Arg 3: Profile name
+	-- API:ImportProfileString(addonName, profileString, optionalCustomProfileName, optionalCallbackFunction)
 	BigWigsAPI:ImportProfileString('LuckyoneUI', (layout == 'main' and profile_main) or profile_healing, (layout == 'main' and name_main) or name_healing)
 
 	-- Handle minimap icon
