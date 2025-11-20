81de0371652aea117dff46acd0ed2b2a7b9ca38e
Luckyone <Luckyone.Development@pm.me>
2025-11-20 13:57:09 +0100

    Implement a helper to safely set DBs

--- LuckyoneUI/Profiles/ShadowLight.lua
+++ LuckyoneUI/Profiles/ShadowLight.lua
@@ -19,8 +19,12 @@ local _G = _G
 -- ElvUI modules
 local E, _, V, P, G = unpack(ElvUI)
 
+local function Set(tbl, path, value)
+	Private:DBSetValue(tbl, path, value)
+end
+
 -- Shadow & Light ProfileDB
--- LC: 09/09/2025
+-- LC: 20/11/2025
 function Private:Setup_ShadowAndLight(installer)
 	if not (Private.IsAddOnLoaded('ElvUI_SLE') and Private.isRetail) then Private:Print('|cff9482c9Shadow & Light|r ' .. L["is not installed or enabled."]) return end
 
@@ -29,92 +33,92 @@ function Private:Setup_ShadowAndLight(installer)
 	E.global.sle = E:CopyTable({}, G.sle)
 
 	-- Global DB
-	E.global.sle.advanced.confirmed = true
-	E.global.sle.advanced.general = true
+	Set(E.global, 'sle.advanced.confirmed', true)
+	Set(E.global, 'sle.advanced.general', true)
 
 	-- Private DB
 	Private:Setup_Private_ShadowAndLight()
 
 	-- Profile DB
-	E.db.sle.actionbar.vehicle.enabled = false
-	E.db.sle.afk.chat.show = false
-	E.db.sle.afk.defaultGraphics.classCrest.enable = false
-	E.db.sle.afk.defaultGraphics.elvuiLogo.enable = false
-	E.db.sle.afk.defaultGraphics.exPack.enable = false
-	E.db.sle.afk.defaultGraphics.factionCrest.enable = false
-	E.db.sle.afk.defaultGraphics.factionLogo.enable = false
-	E.db.sle.afk.defaultGraphics.raceCrest.enable = false
-	E.db.sle.afk.defaultGraphics.slLogo.enable = false
-	E.db.sle.afk.defaultTexts.SL_AFKMessage.anchorPoint = 'CENTER'
-	E.db.sle.afk.defaultTexts.SL_AFKMessage.attachTo = 'SL_BottomPanel'
-	E.db.sle.afk.defaultTexts.SL_AFKMessage.font = Private.Font
-	E.db.sle.afk.defaultTexts.SL_AFKMessage.yOffset = 0
-	E.db.sle.afk.defaultTexts.SL_AFKTimePassed.font = Private.Font
-	E.db.sle.afk.defaultTexts.SL_AFKTimePassed.xOffset = 3
-	E.db.sle.afk.defaultTexts.SL_Date.anchorPoint = 'CENTER'
-	E.db.sle.afk.defaultTexts.SL_Date.font = Private.Font
-	E.db.sle.afk.defaultTexts.SL_Date.size = 26
-	E.db.sle.afk.defaultTexts.SL_Date.xOffset = 0
-	E.db.sle.afk.defaultTexts.SL_Date.yOffset = 40
-	E.db.sle.afk.defaultTexts.SL_GuildName.enable = false
-	E.db.sle.afk.defaultTexts.SL_GuildRank.enable = false
-	E.db.sle.afk.defaultTexts.SL_PlayerClass.enable = false
-	E.db.sle.afk.defaultTexts.SL_PlayerLevel.enable = false
-	E.db.sle.afk.defaultTexts.SL_PlayerName.enable = false
-	E.db.sle.afk.defaultTexts.SL_ScrollFrame.enable = false
-	E.db.sle.afk.defaultTexts.SL_SubText.enable = false
-	E.db.sle.afk.defaultTexts.SL_SubText.font = Private.Font
-	E.db.sle.afk.defaultTexts.SL_Time.font = Private.Font
-	E.db.sle.afk.defaultTexts.SL_Time.size = 26
-	E.db.sle.afk.defaultTexts.SL_Time.yOffset = -10
-	E.db.sle.afk.enable = false
-	E.db.sle.afk.playermodel.enable = false
-	E.db.sle.armory.character.background.overlay = false
-	E.db.sle.armory.character.background.selectedBG = 'HIDE'
-	E.db.sle.armory.character.durability.display = 'Hide'
-	E.db.sle.armory.character.durability.font = Private.Font
-	E.db.sle.armory.character.durability.fontSize = 11
-	E.db.sle.armory.character.enable = true
-	E.db.sle.armory.character.enchant.font = Private.Font
-	E.db.sle.armory.character.enchant.fontSize = 9
-	E.db.sle.armory.character.enchant.showReal = false
-	E.db.sle.armory.character.enchant.yOffset = -1
-	E.db.sle.armory.character.gem.xOffset = 4
-	E.db.sle.armory.character.gem.yOffset = -1
-	E.db.sle.armory.character.ilvl.font = Private.Font
-	E.db.sle.armory.character.ilvl.fontSize = 11
-	E.db.sle.armory.character.transmog.enableArrow = false
-	E.db.sle.armory.inspect.background.overlay = false
-	E.db.sle.armory.inspect.background.selectedBG = 'HIDE'
-	E.db.sle.armory.inspect.enable = true
-	E.db.sle.armory.inspect.enchant.font = Private.Font
-	E.db.sle.armory.inspect.enchant.fontSize = 9
-	E.db.sle.armory.inspect.enchant.showReal = false
-	E.db.sle.armory.inspect.enchant.yOffset = -1
-	E.db.sle.armory.inspect.gem.xOffset = 4
-	E.db.sle.armory.inspect.gem.yOffset = -1
-	E.db.sle.armory.inspect.ilvl.font = Private.Font
-	E.db.sle.armory.inspect.ilvl.fontSize = 11
-	E.db.sle.armory.inspect.transmog.enableArrow = false
-	E.db.sle.armory.stats.itemLevel.font = Private.Font
-	E.db.sle.armory.stats.itemLevel.fontOutline = Private.Outline
-	E.db.sle.armory.stats.itemLevel.fontSize = 18
-	E.db.sle.armory.stats.itemLevel.gradient.style = ''
-	E.db.sle.armory.stats.List.ATTACK_DAMAGE = false
-	E.db.sle.armory.stats.List.MOVESPEED = false
-	E.db.sle.armory.stats.List.SPELLPOWER = false
-	E.db.sle.armory.stats.statHeaders.font = Private.Font
-	E.db.sle.armory.stats.statHeaders.fontOutline = Private.Outline
-	E.db.sle.armory.stats.statLabels.font = Private.Font
-	E.db.sle.armory.stats.statLabels.fontOutline = Private.Outline
-	E.db.sle.armory.stats.statLabels.fontSize = 11
-	E.db.sle.raidmarkers.enable = false
-	E.db.sle.skins.objectiveTracker.classHeader = true
-	E.db.sle.skins.objectiveTracker.underlineClass = true
+	Set(E.db, 'sle.actionbar.vehicle.enabled', false)
+	Set(E.db, 'sle.afk.chat.show', false)
+	Set(E.db, 'sle.afk.defaultGraphics.classCrest.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.elvuiLogo.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.exPack.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.factionCrest.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.factionLogo.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.raceCrest.enable', false)
+	Set(E.db, 'sle.afk.defaultGraphics.slLogo.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKMessage.anchorPoint', 'CENTER')
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKMessage.attachTo', 'SL_BottomPanel')
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKMessage.font', Private.Font)
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKMessage.yOffset', 0)
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKTimePassed.font', Private.Font)
+	Set(E.db, 'sle.afk.defaultTexts.SL_AFKTimePassed.xOffset', 3)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Date.anchorPoint', 'CENTER')
+	Set(E.db, 'sle.afk.defaultTexts.SL_Date.font', Private.Font)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Date.size', 26)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Date.xOffset', 0)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Date.yOffset', 40)
+	Set(E.db, 'sle.afk.defaultTexts.SL_GuildName.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_GuildRank.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_PlayerClass.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_PlayerLevel.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_PlayerName.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_ScrollFrame.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_SubText.enable', false)
+	Set(E.db, 'sle.afk.defaultTexts.SL_SubText.font', Private.Font)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Time.font', Private.Font)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Time.size', 26)
+	Set(E.db, 'sle.afk.defaultTexts.SL_Time.yOffset', -10)
+	Set(E.db, 'sle.afk.enable', false)
+	Set(E.db, 'sle.afk.playermodel.enable', false)
+	Set(E.db, 'sle.armory.character.background.overlay', false)
+	Set(E.db, 'sle.armory.character.background.selectedBG', 'HIDE')
+	Set(E.db, 'sle.armory.character.durability.display', 'Hide')
+	Set(E.db, 'sle.armory.character.durability.font', Private.Font)
+	Set(E.db, 'sle.armory.character.durability.fontSize', 11)
+	Set(E.db, 'sle.armory.character.enable', true)
+	Set(E.db, 'sle.armory.character.enchant.font', Private.Font)
+	Set(E.db, 'sle.armory.character.enchant.fontSize', 9)
+	Set(E.db, 'sle.armory.character.enchant.showReal', false)
+	Set(E.db, 'sle.armory.character.enchant.yOffset', -1)
+	Set(E.db, 'sle.armory.character.gem.xOffset', 4)
+	Set(E.db, 'sle.armory.character.gem.yOffset', -1)
+	Set(E.db, 'sle.armory.character.ilvl.font', Private.Font)
+	Set(E.db, 'sle.armory.character.ilvl.fontSize', 11)
+	Set(E.db, 'sle.armory.character.transmog.enableArrow', false)
+	Set(E.db, 'sle.armory.inspect.background.overlay', false)
+	Set(E.db, 'sle.armory.inspect.background.selectedBG', 'HIDE')
+	Set(E.db, 'sle.armory.inspect.enable', true)
+	Set(E.db, 'sle.armory.inspect.enchant.font', Private.Font)
+	Set(E.db, 'sle.armory.inspect.enchant.fontSize', 9)
+	Set(E.db, 'sle.armory.inspect.enchant.showReal', false)
+	Set(E.db, 'sle.armory.inspect.enchant.yOffset', -1)
+	Set(E.db, 'sle.armory.inspect.gem.xOffset', 4)
+	Set(E.db, 'sle.armory.inspect.gem.yOffset', -1)
+	Set(E.db, 'sle.armory.inspect.ilvl.font', Private.Font)
+	Set(E.db, 'sle.armory.inspect.ilvl.fontSize', 11)
+	Set(E.db, 'sle.armory.inspect.transmog.enableArrow', false)
+	Set(E.db, 'sle.armory.stats.itemLevel.font', Private.Font)
+	Set(E.db, 'sle.armory.stats.itemLevel.fontOutline', Private.Outline)
+	Set(E.db, 'sle.armory.stats.itemLevel.fontSize', 18)
+	Set(E.db, 'sle.armory.stats.itemLevel.gradient.style', '')
+	Set(E.db, 'sle.armory.stats.List.ATTACK_DAMAGE', false)
+	Set(E.db, 'sle.armory.stats.List.MOVESPEED', false)
+	Set(E.db, 'sle.armory.stats.List.SPELLPOWER', false)
+	Set(E.db, 'sle.armory.stats.statHeaders.font', Private.Font)
+	Set(E.db, 'sle.armory.stats.statHeaders.fontOutline', Private.Outline)
+	Set(E.db, 'sle.armory.stats.statLabels.font', Private.Font)
+	Set(E.db, 'sle.armory.stats.statLabels.fontOutline', Private.Outline)
+	Set(E.db, 'sle.armory.stats.statLabels.fontSize', 11)
+	Set(E.db, 'sle.raidmarkers.enable', false)
+	Set(E.db, 'sle.skins.objectiveTracker.classHeader', true)
+	Set(E.db, 'sle.skins.objectiveTracker.underlineClass', true)
 
 	-- To avoid compatibility popup
 	if Private.IsAddOnLoaded('ElvUI_WindTools') then
-		E.db.sle.skins.objectiveTracker.underline = false
+		Set(E.db, 'sle.skins.objectiveTracker.underline', false)
 	end
 
 	if installer then
@@ -125,7 +129,7 @@ function Private:Setup_ShadowAndLight(installer)
 end
 
 -- Shadow & Light PrivateDB
--- LC: 09/09/2025
+-- LC: 20/11/2025
 function Private:Setup_Private_ShadowAndLight()
 
 	-- Get version
@@ -135,11 +139,11 @@ function Private:Setup_Private_ShadowAndLight()
 	E.private.sle = E:CopyTable({}, V.sle)
 
 	-- Private DB
-	E.private.sle.install_complete = version
-	E.private.sle.module.shadows.enable = false
-	E.private.sle.professions.deconButton.enable = false
-	E.private.sle.skins.objectiveTracker.BGbackdrop = false
-	E.private.sle.skins.objectiveTracker.texture = Private.Texture
-	E.private.sle.skins.objectiveTracker.underlineHeight = 2
-	E.private.sle.skins.petbattles.enable = false
+	Set(E.private, 'sle.install_complete', version)
+	Set(E.private, 'sle.module.shadows.enable', false)
+	Set(E.private, 'sle.professions.deconButton.enable', false)
+	Set(E.private, 'sle.skins.objectiveTracker.BGbackdrop', false)
+	Set(E.private, 'sle.skins.objectiveTracker.texture', Private.Texture)
+	Set(E.private, 'sle.skins.objectiveTracker.underlineHeight', 2)
+	Set(E.private, 'sle.skins.petbattles.enable', false)
 end
