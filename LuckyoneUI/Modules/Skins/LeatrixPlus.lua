c9b6eb674635f8cbca0e00c286822926263d4113
Luckyone <Luckyone.Development@pm.me>
2026-01-17 17:57:17 +0100

    Cleanup

--- LuckyoneUI/Modules/Skins/LeatrixPlus.lua
+++ LuckyoneUI/Modules/Skins/LeatrixPlus.lua
@@ -10,6 +10,9 @@ end
 local pairs = pairs
 local unpack = unpack
 
+-- API cache
+local C_Timer = C_Timer
+
 -- ElvUI modules
 local E = unpack(ElvUI)
 local S = E:GetModule('Skins')
@@ -17,15 +20,13 @@ local S = E:GetModule('Skins')
 function Private:Skin_LeatrixPlus()
 	if not (Private.isClassic or Private.isTBC) and not Private.Addon.db.profile.skins.LeatrixPlus then return end
 
-	C_Timer.After(1, function() -- ToDo: Check back for a better method
-		for _, checkbox in pairs({ PaperDollFrame:GetChildren() }) do
-			-- Find the head/cloak toggle checkboxes on the character frame
-			if checkbox:IsObjectType('CheckButton') and not checkbox.isSkinned then
-				S:HandleCheckBox(checkbox)
-				checkbox.isSkinned = true
-			end
+	for _, checkbox in pairs({ PaperDollFrame:GetChildren() }) do
+		-- Find the head/cloak toggle checkboxes on the character frame
+		if checkbox:IsObjectType('CheckButton') and not checkbox.isSkinned then
+			S:HandleCheckBox(checkbox)
+			checkbox.isSkinned = true
 		end
-	end)
+	end
 end
 
-S:AddCallbackForAddon('Leatrix_Plus', 'LuckyoneUI_LeatrixPlus', Private.Skin_LeatrixPlus)
+S:AddCallbackForAddon('Leatrix_Plus', 'LuckyoneUI_LeatrixPlus', C_Timer.After(1, function() Private.Skin_LeatrixPlus() end))
--- LuckyoneUI/Modules/Skins/WhatsTraining.lua
+++ LuckyoneUI/Modules/Skins/WhatsTraining.lua
@@ -19,29 +19,27 @@ local S = E:GetModule('Skins')
 function Private:Skin_WhatsTraining()
 	if not (Private.isClassic or Private.isTBC) and not Private.Addon.db.profile.skins.WhatsTraining then return end
 
-	C_Timer.After(1, function() -- ToDo: Check back for a better method
-		if WhatsTrainingFrame and not WhatsTrainingFrame.isSkinned then
-			-- Main Frame
-			S:HandleFrame(WhatsTrainingFrame, true, true, 11, -50, -32, 76)
+	if WhatsTrainingFrame and not WhatsTrainingFrame.isSkinned then
+		-- Main Frame
+		S:HandleFrame(WhatsTrainingFrame, true, true, 11, -50, -32, 76)
 
-			-- Scroll Bar
-			S:HandleScrollBar(WhatsTrainingFrameScrollBarScrollBar)
+		-- Scroll Bar
+		S:HandleScrollBar(WhatsTrainingFrameScrollBarScrollBar)
 
-			-- Edit Box
-			S:HandleEditBox(WhatsTrainingFrameSearchBox, 160)
-			WhatsTrainingFrameSearchBox:Height(24)
-			WhatsTrainingFrameSearchBox:Point('TOPLEFT', WhatsTrainingFrame, 'TOPLEFT', 15, -50)
+		-- Edit Box
+		S:HandleEditBox(WhatsTrainingFrameSearchBox, 160)
+		WhatsTrainingFrameSearchBox:Height(24)
+		WhatsTrainingFrameSearchBox:Point('TOPLEFT', WhatsTrainingFrame, 'TOPLEFT', 15, -50)
 
-			-- Frame Positioning
-			WhatsTrainingFrame:Point('TOPLEFT', SpellBookFrame, 'TOPLEFT', 0, 38)
-			WhatsTrainingFrame:Point('BOTTOMRIGHT', SpellBookFrame, 'BOTTOMRIGHT', 0, 0)
+		-- Frame Positioning
+		WhatsTrainingFrame:Point('TOPLEFT', SpellBookFrame, 'TOPLEFT', 0, 38)
+		WhatsTrainingFrame:Point('BOTTOMRIGHT', SpellBookFrame, 'BOTTOMRIGHT', 0, 0)
 
-			-- Close Button
-			SpellBookCloseButton:SetFrameStrata('HIGH')
+		-- Close Button
+		SpellBookCloseButton:SetFrameStrata('HIGH')
 
-			WhatsTrainingFrame.isSkinned = true
-		end
-	end)
+		WhatsTrainingFrame.isSkinned = true
+	end
 end
 
-S:AddCallbackForAddon('WhatsTraining', 'LuckyoneUI_WhatsTraining', Private.Skin_WhatsTraining)
+S:AddCallbackForAddon('WhatsTraining', 'LuckyoneUI_WhatsTraining', C_Timer.After(1, function() Private.Skin_WhatsTraining() end))
