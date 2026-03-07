56c801e6b3f50aea0d42f52dcc52def3cad6f778
Luckyone <Luckyone.Development@pm.me>
2026-03-07 23:06:07 +0100

    AuraWatch Mistweaver

--- LuckyoneUI/Modules/Auras/Filters.lua
+++ LuckyoneUI/Modules/Auras/Filters.lua
@@ -299,12 +299,12 @@ function Private:Setup_Filters_Retail()
 
 	-- Monk
 	classes['MONK'][115175] = { -- Soothing Mist
-		['point'] = 'BOTTOMLEFT',
-		['xOffset'] = -1,
+		['point'] = 'TOPRIGHT',
 		['displayText'] = true,
-		['yOffset'] = -1,
+		['yOffset'] = 1,
 		['sizeOffset'] = 2,
 		['style'] = 'texturedIcon',
+		['xOffset'] = 1,
 	}
 	classes['MONK'][119611] = { -- Renewing Mist
 		['displayText'] = true,
