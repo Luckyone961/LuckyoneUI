c57599254066edfd4f62dc80e9e54e1b98cc4f04
Luckyone <Luckyone.Development@pm.me>
2026-08-17 12:21:26 +0200

    Count offsets

--- LuckyoneUI/Profiles/ElvUI.lua
+++ LuckyoneUI/Profiles/ElvUI.lua
@@ -771,7 +771,8 @@ function Private:Setup_ElvUI(layout)
 	E.db.auras.buffs.countFont = Private.Font
 	E.db.auras.buffs.countFontOutline = Private.Outline
 	E.db.auras.buffs.countFontSize = 11
-	E.db.auras.buffs.countYOffset = 12
+	E.db.auras.buffs.countXOffset = 1
+	E.db.auras.buffs.countYOffset = 13
 	E.db.auras.buffs.fadeThreshold = -1
 	E.db.auras.buffs.horizontalSpacing = 2
 	E.db.auras.buffs.seperateOwn = 0
@@ -781,7 +782,8 @@ function Private:Setup_ElvUI(layout)
 	E.db.auras.debuffs.countFont = Private.Font
 	E.db.auras.debuffs.countFontOutline = Private.Outline
 	E.db.auras.debuffs.countFontSize = 11
-	E.db.auras.debuffs.countYOffset = 12
+	E.db.auras.debuffs.countXOffset = 1
+	E.db.auras.debuffs.countYOffset = 13
 	E.db.auras.debuffs.fadeThreshold = -1
 	E.db.auras.debuffs.horizontalSpacing = 2
 	E.db.auras.debuffs.maxWraps = 2
