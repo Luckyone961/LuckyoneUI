a8b445690923bb282f48cf90de76a3fa8db4a270
Luckyone <Luckyone.Development@pm.me>
2026-09-14 10:15:59 +0200

    Update LBB skin

--- LuckyoneUI/Modules/Skins/Addons/LFGBulletinBoard.lua
+++ LuckyoneUI/Modules/Skins/Addons/LFGBulletinBoard.lua
@@ -4,47 +4,78 @@ if not Private.ElvUI then
 	return
 end
 
+local next = next
 local unpack = unpack
+
+local hooksecurefunc = hooksecurefunc
+
+local _G = _G
 
 local E = unpack(ElvUI)
 local S = E:GetModule('Skins')
 
+local function LayoutTabs(frame, position)
+	local lastTab
+	for _, tab in next, frame.Tabs do
+		tab:ClearAllPoints()
+
+		if not lastTab then
+			if position == 'top' then
+				tab:Point('BOTTOMLEFT', frame, 'TOPLEFT', -10, 0)
+			else
+				tab:Point('TOPLEFT', frame, 'BOTTOMLEFT', -10, 0)
+			end
+		else
+			tab:Point('TOPLEFT', lastTab, 'TOPRIGHT', -19, 0)
+		end
+
+		lastTab = tab
+	end
+end
+
 local function Skin_LFGBulletinBoard()
 	if not Private.Addon.db.profile.skins.LFGBulletinBoard then return end
 
-	local frame = GroupBulletinBoardFrame
-	if not frame or frame.isSkinned then return end
+	local addon = _G.GroupBulletinBoard_Addon
 
 	-- Main Frame
+	local frame = _G.GroupBulletinBoardFrame
 	S:HandleFrame(frame)
+	_G.GroupBulletinBoardFrameBackdrop:Kill()
 
 	-- Tabs
-	local tab1 = GroupBulletinBoardFrameTab1
-	S:HandleTab(tab1)
-	tab1:ClearAllPoints()
-	tab1:Point('TOPLEFT', frame, 'BOTTOMLEFT', -10, 0)
-
-	local tab2 = GroupBulletinBoardFrameTab2
-	if tab2 then
-		S:HandleTab(tab2)
-		tab2:ClearAllPoints()
-		tab2:Point('LEFT', tab1, 'RIGHT', -19, 0)
+	for _, tab in next, frame.Tabs do
+		S:HandleTab(tab)
 	end
 
-	-- Edit Box
-	local filter = GroupBulletinBoardFrameResultsFilter
+	LayoutTabs(frame, frame.Tabs[1].position)
+	hooksecurefunc(addon.Tool, 'ChangeTabPositions', LayoutTabs)
+
+	-- Header
+	local header = _G.GroupBulletinBoardFrameHeaderContainer
+	for _, button in next, { header.CloseButton, header.SettingsButton, header.RefreshButton } do
+		S:HandleButton(button)
+	end
+
+	-- Footer
+	local footer = _G.GroupBulletinBoardFrameFooterContainer
+	S:HandleButton(footer.AnnounceChannelSelect)
+	S:HandleButton(footer.AnnounceButton)
+	S:HandleEditBox(footer.AnnounceInput)
+
+	-- Chat Requests Tab
+	local filter = _G.GroupBulletinBoardFrameResultsFilter
 	S:HandleEditBox(filter)
 	filter:Height(20)
 
-	-- Close Button
-	local closeButton = GroupBulletinBoardFrameHeaderContainerCloseButton
-	S:HandleCloseButton(closeButton)
+	_G.GroupBulletinBoardFrame_ScrollFrameBackdrop:Kill()
+	S:HandleScrollBar(_G.GroupBulletinBoardFrame_ScrollFrameScrollBar)
 
-	-- Scroll Bars
-	S:HandleScrollBar(GroupBulletinBoardFrame_ScrollFrameScrollBar)
-	S:HandleTrimScrollBar(LFGBulletinBoardLFGToolFrame.scrollBar)
-
-	frame.isSkinned = true
+	-- LFG Tool Tab
+	local tool = addon.LfgTool
+	tool.CategoryButton:Height(20)
+	S:HandleDropDownBox(tool.CategoryButton.Dropdown, 150)
+	S:HandleTrimScrollBar(tool.ScrollContainer.scrollBar)
 end
 
 S:AddCallbackForAddon('LFGBulletinBoard', 'LuckyoneUI_LFGBulletinBoard', Skin_LFGBulletinBoard)
